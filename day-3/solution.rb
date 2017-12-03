e42043cffb7f96dbe10614044f5ed71775cd4954
Logan McDonald <loganmcdona11@gmail.com>
2017-12-03 01:47:28 -0500

    very bad part 2 day 3

--- day-3/solution.rb
+++ day-3/solution.rb
@@ -6,6 +6,8 @@ def create_spiral(num)
   x = y = 0
   dx, dy = 0, -1
   coords = Array.new
+  coords_hash = Hash.new
+  value = nil
 
   (array_dim ** 2).times do
     if x.abs == y.abs && [dx,dy] != [1,0] or x > 0 && y == 1 - x
@@ -17,22 +19,57 @@ def create_spiral(num)
       x, y = -y + dx, x + dy
     end
 
+    if value == nil
+      value = 1
+    elsif value > num
+      value = value
+    else
+      value = find_surrounding_coords_value([x, y], coords_hash)
+    end
+
     coords << [x, y]
+    coords_hash[[x, y]] = value
     x, y = x + dx, y + dy
   end
-  return coords
+  return coords_hash
 end
 
-def find_coord(num)
-  spiral = create_spiral(num)
-  spiral[num - 1]
+def find_surrounding_coords_value(coord, coords_hash)
+  sum = 0
+  i, j = coord[0], coord[1]
+  surrounding_coords = [
+    [i-1, j],
+    [i, j-1],
+    [i-1, j-1],
+    [i+1, j],
+    [i, j+1],
+    [i+1, j+1],
+    [i+1, j-1],
+    [i-1, j+1]
+  ]
+
+  surrounding_coords.each do |arr|
+    if !coords_hash[arr].nil?
+      sum += coords_hash[arr]
+    end
+  end
+  return sum
 end
+
+def answer(num)
+  spiral = create_spiral(num)
+  return [spiral.keys[num - 1], spiral.values[num - 1]]
+end
+
 
 def find_shorest_distance(point1, point2)
   (point1[0] - point2[0]).abs + (point1[1] - point2[1]).abs
 end
 
 middle_cord = [0, 0]
-point = find_coord(input)
+answer = answer(input)
+point = answer.first
+value = answer.last
 
 puts find_shorest_distance(middle_cord, point)
+puts value
