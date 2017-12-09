829aa584591cd2c82112399a9efbe6ea282bf097
Logan McDonald <loganmcdona11@gmail.com>
2017-12-09 09:31:50 -0500

    day 9 both parts

--- 2017/day-9/solution.rb
+++ 2017/day-9/solution.rb
@@ -1,37 +1,49 @@
 require 'test/unit'
 extend Test::Unit::Assertions
 
-def calculate_score(chars)
-  total_score = 0
-  group_score = 1
-  i = 0
+def calculate_scores(stream)
+  total_score, level_score, gc, garbage, bang = 0, 0, 0, false, false
 
-  chars.length.times do
-    case chars[i]
-    when '!'
-      i += 2
-    when '{'
-      total_score += group_score
-      group_score += 1
-      i += 1
-    when '}'
-      group_score -= 1
-      i += 1
+  stream.chars do |char|
+    if bang then next bang = false end
+    if garbage
+      case char
+      when '!'
+        bang = true
+      when '>'
+        garbage = false
+      else
+        if garbage then gc += 1 end
+      end
     else
-      i += 1
+      case char
+      when '{'
+        total_score += level_score += 1
+      when '}'
+        level_score -= 1
+      when '<'
+        garbage = true
+      end
     end
   end
-  return total_score
+  return total_score, gc
 end
 
-assert_equal calculate_score('{}'), 1
-assert_equal calculate_score('{{{}}}'), 6
-assert_equal calculate_score('{{},{}}'), 5
-assert_equal calculate_score('{{{},{},{{}}}}'), 16
-assert_equal calculate_score('{<a>,<a>,<a>,<a>}'), 1
-assert_equal calculate_score('{{<ab>},{<ab>},{<ab>},{<ab>}}'), 9
-assert_equal calculate_score('{{<!!>},{<!!>},{<!!>},{<!!>}}'), 9
-assert_equal calculate_score('{{<a!>},{<a!>},{<a!>},{<ab>}}'), 3
+assert_equal calculate_scores('{}').first, 1
+assert_equal calculate_scores('{{{}}}').first, 6
+assert_equal calculate_scores('{{},{}}').first, 5
+assert_equal calculate_scores('{{{},{},{{}}}}').first, 16
+assert_equal calculate_scores('{<a>,<a>,<a>,<a>}').first, 1
+assert_equal calculate_scores('{{<ab>},{<ab>},{<ab>},{<ab>}}').first, 9
+assert_equal calculate_scores('{{<!!>},{<!!>},{<!!>},{<!!>}}').first, 9
+assert_equal calculate_scores('{{<a!>},{<a!>},{<a!>},{<ab>}}').first, 3
+assert_equal calculate_scores('<>').last, 0
+assert_equal calculate_scores('<random characters>').last, 17
+assert_equal calculate_scores('<<<<>').last, 3
+assert_equal calculate_scores('<{!>}>').last, 2
+assert_equal calculate_scores('<!!>').last, 0
+assert_equal calculate_scores('<!!!>>').last, 0
+assert_equal calculate_scores('<{o"i!a,<{i<a>').last, 10
 
-chars = File.open("./input.txt").read.split('')
-calculate_score(chars)
+stream = File.open("./input.txt").read
+puts calculate_scores(stream)
