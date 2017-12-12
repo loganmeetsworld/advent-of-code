9cc5e62362b24219908691ff9bd2d9c10c498a4f
Logan McDonald <loganmcdona11@gmail.com>
2017-12-12 07:10:04 -0500

    part 1

--- 2017/day-12/solution.rb
+++ 2017/day-12/solution.rb
@@ -1,1 +1,26 @@
-input = File.open('input.txt').read
+require 'set'
+input = File.open('input.txt').read.split("\n")
+program_hash = Hash.new
+input.map{ |x| program_hash[x.split(' <-> ')[0]] = x.split(' <-> ')[1].scan(/(\d+)/).flatten }
+initial_program = '0'
+
+def answer(initial_program, program_hash)
+  program_set = Set.new
+  program_set.add(initial_program)
+  next_programs = program_hash[initial_program]
+
+  while !next_programs.empty?
+    next_program = next_programs.shift
+    next_program_connections = program_hash[next_program]
+
+    if next_program_connections.find{ |pcs| program_set.include?(pcs) } && !program_set.include?(next_program)
+      program_set.add(next_program)
+      next_programs += next_program_connections
+    end
+  end
+  return program_set
+end
+
+program_set = answer(initial_program, program_hash)
+
+puts "Part 1: #{program_set.length}"
