7e9ed3baf073d891bd21b57a532f226b10c08b67
Logan McDonald <loganmcdona11@gmail.com>
2017-12-14 10:58:22 -0800

    better part 1 for part 2

--- 2017/day-14/solution.rb
+++ 2017/day-14/solution.rb
@@ -6,18 +6,11 @@ GRID_SIZE = 128
 
 def knot_hash(input); day10_part2_answer(input); end
 
-def create_map(key_string)
-  map = ''
-  (0...GRID_SIZE).each do |i|
-    map += knot_hash("#{key_string}-#{i.to_s}").to_i(16).to_s(2) + "\n"
-  end
-  map
+def knot_hash_map(key_string, map)
+  (0...GRID_SIZE).each{ |i| map += knot_hash("#{key_string}-#{i.to_s}").to_i(16).to_s(2) + "\n" }
+  map.gsub!('0', '.').gsub!('1', '#')
 end
 
-def part1_answer(key_string)
-  create_map(key_string).count('1')
-end
-
-assert_equal part1_answer('flqrgnkx'), 8108
-part_1 = part1_answer('nbysizxe')
+assert_equal knot_hash_map('flqrgnkx', '').count('#'), 8108
+part_1 = knot_hash_map('nbysizxe', '').count('#')
 puts "Part 1: #{part_1}"
