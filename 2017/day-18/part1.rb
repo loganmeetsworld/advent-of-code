0ff9ef6149c7c3d7a5d1d84b41369ec904c0f6a2
Logan McDonald <loganmcdona11@gmail.com>
2017-12-18 17:45:30 -0800

    wip

--- 2017/day-18/part1.rb
+++ 2017/day-18/part1.rb
@@ -1,30 +1,29 @@
-def answer(registers)
-  values, current_position, freq = {}, 0, []
-  registers.map{|x| values[x.split(' ')[1]] = 0}
+def run(registers)
+  current_position, values, freq = 0, {}, []
+  registers.map{|x| values[x.split[1]] = 0}
 
   until current_position > registers.length
-    i, x, y = registers[current_position].split(' ')[0], registers[current_position].split(' ')[1], registers[current_position].split(' ')[2]
-     y = values[y] if y.to_i.to_s != y if y
+    i, x, y = registers[current_position].split[0], registers[current_position].split[1], registers[current_position].split[2]
+    y = values[y] if y.to_i.to_s != y if y; y = y.to_i
     case i
     when 'snd'
-      freq << values[x]; current_position += 1
+      freq << values[x]
     when 'set'
-      values[x] = y.to_i; current_position += 1
+      values[x] = y
     when 'add'
-      values[x] += y.to_i; current_position += 1
+      values[x] += y
     when 'mul'
-      values[x] *= y.to_i; current_position += 1
+      values[x] *= y
     when 'mod'
-      values[x] = values[x] % y.to_i; current_position += 1
+      values[x] = values[x] % y
     when 'rcv'
-      if freq[-1] > 0 then return freq; break else current_position += 1 end
+      if freq[-1] > 0 then break freq end
     when 'jgz'
-      values[x] > 0 ? current_position = (current_position + y.to_i) % registers.length : current_position += 1
+      current_position = (current_position + y) % registers.length - 1 if values[x] > 0
     end
+    current_position += 1
   end
 end
 
-registers = File.open('input.txt').read.split("\n")
-puts "Part 1: #{answer(registers)[-1]}"
-
-sound = []
+registers = File.open('input.txt').read.lines
+puts "Part 1: #{run(registers).last}"
--- 2017/day-18/part2.rb
+++ 2017/day-18/part2.rb
@@ -1,2 +1,26 @@
-input = File.open('input.txt').read.split("\n")
+def run(registers, id, freq, received)
+  current_position, values, vals_received = 0, {}, 0
+  registers.map{|x| values[x.split[1]] = 0}; values[:p] = id
 
+  while current_position >= 0
+    i, x, y = registers[current_position].split[0], registers[current_position].split[1], registers[current_position].split[2]
+    y = values[y] if y.to_i.to_s != y if y; y = y.to_i
+    case i
+    when 'snd'
+      freq << values[x]
+    when 'set'
+      values[x] = y
+    when 'add'
+      values[x] += y
+    when 'mul'
+      values[x] *= y
+    when 'mod'
+      values[x] = values[x] % y
+    when 'rcv'
+      if freq[-1] > 0 then break freq end
+    when 'jgz'
+      current_position = (current_position + y) % registers.length - 1 if values[x] > 0
+    end
+    current_position += 1
+  end
+end
