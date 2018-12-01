b1db5ec88bc141eb25850294d4f089a3632af2f2
Logan McDonald <loganmcdona11@gmail.com>
2018-12-01 15:35:13 +0000

    2018/day-1: refactors to ruby code, use a set like a set

--- 2018/day-1/solution.rb
+++ 2018/day-1/solution.rb
@@ -3,15 +3,12 @@ puts File.open("./input.txt").readlines.map(&:to_i).inject(&:+)
 
 puts "Part 2:"
 require 'set'
-changes = File.open("./input.txt").readlines
-current_freq = 0
-freq_tracker = Set.new([current_freq])
-found = nil
+changes, current_freq, freq_tracker, found = File.open("./input.txt").readlines, 0, Set.new([current_freq]), nil
 
 until found
     for c in changes
         current_freq += c.to_i
-        if freq_tracker.include?(current_freq)
+        if freq_tracker[current_freq]
             found = current_freq
             break
         end
