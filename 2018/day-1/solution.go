9c53eafb49122467389847830393540aa7497204
Logan McDonald <loganmcdona11@gmail.com>
2018-12-01 15:45:27 +0000

    2018/day-1: go part 2

--- 2018/day-1/solution.go
+++ 2018/day-1/solution.go
@@ -7,7 +7,7 @@ import (
 	"strings"
 )
 
-func part1(input string) string {
+func partOne(input string) string {
 	inputList := strings.Split(input, "\n")
 	var result int
 
@@ -18,9 +18,27 @@ func part1(input string) string {
 	return strconv.Itoa(result)
 }
 
+func partTwo(input string) string {
+	inputList := strings.Split(input, "\n")
+	var currentFreq int
+	var tracker = make(map[int]bool)
+
+	for {
+		for i := range inputList {
+			num, _ := strconv.Atoi(inputList[i])
+			currentFreq += num
+			if tracker[currentFreq] {
+				return strconv.Itoa(currentFreq)
+			}
+			tracker[currentFreq] = true
+		}
+	}
+}
+
 func main() {
 	inputBytes, _ := ioutil.ReadFile("input.txt")
 	input := string(inputBytes)
 
-	fmt.Println("Part 1 answer : ", part1(input))
+	fmt.Println("Part 1 answer : ", partOne(input))
+	fmt.Println("Part 1 answer : ", partTwo(input))
 }
