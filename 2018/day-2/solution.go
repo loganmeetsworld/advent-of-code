14e1ab790f2e46efd5f76604851ce546086b814f
Logan McDonald <loganmcdona11@gmail.com>
2018-12-02 11:30:32 -0500

    2018/day-2: part 1 in go

--- 2018/day-2/solution.go
+++ 2018/day-2/solution.go
@@ -3,12 +3,37 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
+	"strings"
 )
 
 func partOne(input string) string {
-	// inputList := strings.Split(input, "\n")
+	inputList := strings.Split(input, "\n")
+	var twos int
+	var threes int
 
-	return input
+	for i := range inputList {
+		var freqHash = make(map[string]int)
+		box := strings.Split(inputList[i], "")
+		for j := range box {
+			freqHash[box[j]]++
+		}
+		twoFound := false
+		threeFound := false
+
+		for _, v := range freqHash {
+			if v == 2 && !twoFound {
+				twoFound = true
+				twos++
+			}
+			if v == 3 && !threeFound {
+				threeFound = true
+				threes++
+			}
+		}
+	}
+
+	return strconv.Itoa(twos * threes)
 }
 
 func partTwo(input string) string {
@@ -22,5 +47,5 @@ func main() {
 	input := string(inputBytes)
 
 	fmt.Println("Part 1 answer : ", partOne(input))
-	fmt.Println("Part 1 answer : ", partTwo(input))
+	// fmt.Println("Part 1 answer : ", partTwo(input))
 }
