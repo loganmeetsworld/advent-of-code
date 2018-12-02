5e423b072a33b072e807785873a770a24610699a
Logan McDonald <loganmcdona11@gmail.com>
2018-12-02 09:00:58 -0500

    2018/day-2: ruby part 1

--- 2018/day-2/solution.rb
+++ 2018/day-2/solution.rb
@@ -2,4 +2,13 @@ input = File.open("./input.txt").readlines
 
 puts "Part 1:"
 
+twos, threes = 0, 0
+input.each do |box|
+    freq_hash = box.split('').each_with_object(Hash.new(0)) { |o, h| h[o] += 1 }
+    twos += 1 if freq_hash.values.include?(2)
+    threes += 1 if freq_hash.values.include?(3)
+end
+
+puts twos * threes
+
 puts "Part 2:"
