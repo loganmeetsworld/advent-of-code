fe48924f247e5f69e0fdc6206945af6d2c4dc398
Logan McDonald <loganmcdona11@gmail.com>
2018-12-04 10:13:40 -0500

    2018/day-4: part 2 in ruby

--- 2018/day-4/solution.rb
+++ 2018/day-4/solution.rb
@@ -3,9 +3,7 @@ require 'date'
 input = File.open("./input.txt").readlines
 
 def calculate_sleep(watch_events)
-    # always going to start out awake on shift
-    asleep_time = 0
-    asleep_minutes = []
+    asleep_time, asleep_minutes = 0, []
     times = watch_events.join("\n").scan(/\d{2}:(\d{2})/).flatten.map(&:to_i).each_slice(2).to_a
     times.each do |time|
         if time.length == 2
@@ -13,7 +11,6 @@ def calculate_sleep(watch_events)
             asleep_minutes += (time[0]...time[1]).to_a
         end
     end
-
     return asleep_time, asleep_minutes
 end
 
@@ -33,9 +30,12 @@ parsed_watches = grouped_watches.map{ |x| parse(x) }
 grouped_parsed_watches = parsed_watches.group_by{ |x| x[:id] }
 sleep_totals = []
 grouped_parsed_watches.values.each do |group|
-    total_sleep = group.map { |h| h[:asleep_time] }.inject(:+)
+    total_sleep = group.map{ |h| h[:asleep_time] }.inject(:+)
+    total_minutes = group.map{ |h| h[:asleep_minutes] }.flatten
+    most_repeated_minute = total_minutes.max_by { |i| total_minutes.count(i) }
+    repeated_count = total_minutes.count(most_repeated_minute)
     id = group[0][:id]
-    sleep_totals << {:id => id, :total_sleep => total_sleep}
+    sleep_totals << {:id => id, :total_sleep => total_sleep, :repeated_count => repeated_count, :most_repeated_minute => most_repeated_minute}
 end
 
 puts "Part 1:"
@@ -46,3 +46,5 @@ minute_most_asleep = minutes.max_by { |i| minutes.count(i) }
 puts minute_most_asleep * biggest_sleeper_id.to_i
 
 puts "Part 2:"
+biggest_sleeper_by_minute = sleep_totals.max_by{ |x| x[:repeated_count] }
+puts biggest_sleeper_by_minute[:id].to_i * biggest_sleeper_by_minute[:most_repeated_minute]
