26595e497b69133da6858b97e967a60c56c09483
Logan McDonald <loganmcdona11@gmail.com>
2018-12-04 10:28:01 -0500

    2018/day-4: slightly refactored for both answers

--- 2018/day-4/solution.rb
+++ 2018/day-4/solution.rb
@@ -1,7 +1,3 @@
-require 'date'
-
-input = File.open("./input.txt").readlines
-
 def calculate_sleep(watch_events)
     asleep_time, asleep_minutes = 0, []
     times = watch_events.join("\n").scan(/\d{2}:(\d{2})/).flatten.map(&:to_i).each_slice(2).to_a
@@ -14,37 +10,34 @@ def calculate_sleep(watch_events)
     return asleep_time, asleep_minutes
 end
 
-def parse(watch)
+def parse_watch(watch)
     id = watch.scan(/#(\d+)/).flatten.first
-    date = watch.scan(/\[(\d{4}-\d{2}-\d{2})/).flatten.last
-    watch_events = watch.split("\n")
-    asleep_time, asleep_minutes = calculate_sleep(watch_events[1..-1])
-
-    return {:id => id, :date => date, :asleep_time => asleep_time, :asleep_minutes => asleep_minutes}
+    asleep_time, asleep_minutes = calculate_sleep(watch.split("\n")[1..-1])
+    return {:id => id, :asleep_time => asleep_time, :asleep_minutes => asleep_minutes}
 end
 
-sorted_watches = input.map(&:chomp).sort
-grouped_watches = sorted_watches.join("\n").split(/(?=\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}\] Guard)/)
-parsed_watches = grouped_watches.map{ |x| parse(x) }
-
-grouped_parsed_watches = parsed_watches.group_by{ |x| x[:id] }
-sleep_totals = []
-grouped_parsed_watches.values.each do |group|
+def parse_sleep_info(id, group)
     total_sleep = group.map{ |h| h[:asleep_time] }.inject(:+)
     total_minutes = group.map{ |h| h[:asleep_minutes] }.flatten
     most_repeated_minute = total_minutes.max_by { |i| total_minutes.count(i) }
     repeated_count = total_minutes.count(most_repeated_minute)
-    id = group[0][:id]
-    sleep_totals << {:id => id, :total_sleep => total_sleep, :repeated_count => repeated_count, :most_repeated_minute => most_repeated_minute}
+    return {:id => id, :total_sleep => total_sleep, :repeated_count => repeated_count, :most_repeated_minute => most_repeated_minute}
 end
 
+sleep_info = File.open("./input.txt")
+    .readlines
+    .map(&:chomp)
+    .sort
+    .join("\n")
+    .split(/(?=\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}\] Guard)/)
+    .map{ |w| parse_watch(w) }
+    .group_by{ |w| w[:id] }
+    .map{ |id, group| parse_sleep_info(id, group) }
+
 puts "Part 1:"
-biggest_sleeper_id = sleep_totals.max_by{ |x| x[:total_sleep] }[:id]
-biggest_sleeper = grouped_parsed_watches[biggest_sleeper_id]
-minutes = biggest_sleeper.map{ |x| x[:asleep_minutes]}.flatten
-minute_most_asleep = minutes.max_by { |i| minutes.count(i) }
-puts minute_most_asleep * biggest_sleeper_id.to_i
+biggest_sleeper_by_total_sleep = sleep_info.max_by{ |x| x[:total_sleep] }
+puts  biggest_sleeper_by_total_sleep[:id].to_i * biggest_sleeper_by_total_sleep[:most_repeated_minute]
 
 puts "Part 2:"
-biggest_sleeper_by_minute = sleep_totals.max_by{ |x| x[:repeated_count] }
+biggest_sleeper_by_minute = sleep_info.max_by{ |x| x[:repeated_count] }
 puts biggest_sleeper_by_minute[:id].to_i * biggest_sleeper_by_minute[:most_repeated_minute]
