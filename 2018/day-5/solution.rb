09e013da03e998d2ff597bd85fe726a2765a60a0
Logan McDonald <loganmcdona11@gmail.com>
2018-12-05 08:19:55 -0500

    2018/day-5: solution in ruby p1 that runs very slow

--- 2018/day-5/solution.rb
+++ 2018/day-5/solution.rb
@@ -1,7 +1,26 @@
-input = 'dabAcCaCBAcCcaDA'
-# input = File.open("./input.txt").read.chomp.freeze
+# input = 'dabAcCaCBAcCcaDA'
+input = File.open("./input.txt").read.chomp
+
+def polymer?(s1, s2)
+    return false if /[[:upper:]]/.match(s1) and /[[:upper:]]/.match(s2)
+    return if s1 == nil or s2 == nil
+    s1 == s2.upcase || s2 == s1.upcase
+end
 
 puts "Part 1:"
+while true
+    length = input.length
+    for i in (0...input.length).to_a
+        if polymer?(input[i], input[i + 1])
+            input.slice!(input[i] + input[i + 1])
+            break
+        end
+    end
+    if input.length == length
+        break
+    end
+end
 
+puts input.length
 
 puts "Part 2:"
