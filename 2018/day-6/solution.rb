2e5f7d951757208c1fa8594ae78f9cfd20f989a8
Logan McDonald <loganmcdona11@gmail.com>
2018-12-07 09:48:27 -0500

    2018/day-6: part 2 complete

--- 2018/day-6/solution.rb
+++ 2018/day-6/solution.rb
@@ -7,6 +7,8 @@ def find_shortest_distance(p1, p2)
 end
 
 puts 'Part 1:'
+region_size = 10000
+region_count = 0
 coords = File.open('input.txt').readlines.map{ |c| c.split(', ').map(&:to_i) }
 max_x, max_y = dimensions(coords)
 area_counts = Array.new(coords.size, 0)
@@ -15,10 +17,19 @@ infinite_coords = []
 max_x.times do |x|
   max_y.times do |y|
     distances = {}
+    total_dist = 0
     point = [x, y]
-    coords.each_with_index{ |coord, idx| distances[idx] = find_shortest_distance(point, coord) }
+    coords.each_with_index do |coord, idx|
+      dist = find_shortest_distance(point, coord)
+      distances[idx] = dist
+      total_dist += dist
+    end
     min_coords = distances.select { |idx, dist| dist == distances.values.min }
     min_coord_idx = min_coords.keys.first
+
+    if total_dist < region_size
+      region_count += 1
+    end
 
     if min_coords.size == 1
       area_counts[min_coord_idx] += 1
@@ -33,3 +44,6 @@ end
 infinite_coords.each{ |i| area_counts[i] = 0 }
 
 puts area_counts.max
+
+puts "Part 2:"
+puts region_count
