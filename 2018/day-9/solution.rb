4cabe53d667757b289df8e2b9c2e031afe2ae527
Logan McDonald <loganmcdona11@gmail.com>
2018-12-09 19:35:54 -0500

    2018/day-9: ruby parts 1 and 2

--- 2018/day-9/solution.rb
+++ 2018/day-9/solution.rb
@@ -1,2 +1,35 @@
-input = "413 players; last marble is worth 71082 points"
-players, points = input.scan(\s(\d+)\).first
+def answer(players, points)
+    current_pos, marble, count, player_scores, board, stop = 0, 0, 0, Hash.new(0), [], false
+    while !stop
+        (0...players).each do |player|
+            if marble % 23 == 0 && marble != 0
+                player_scores[player] += marble
+                board = board.rotate(current_pos - 7)
+                player_scores[player] += board.shift
+                current_num = board[0]
+                board = board.rotate(board.index(0))
+                current_pos = board.index(current_num)
+                count += 1
+            else
+                board = board.rotate(current_pos + 2)
+                board.push(marble)
+                board = board.rotate(board.index(0))
+                current_pos = board.index(marble)
+            end
+            marble += 1
+            stop = true if marble >= points
+        end
+    end
+    return player_scores.values.max
+end
+
+input = File.open('input.txt').read
+players, points = input.scan(/(\d+)/).map(&:first).map(&:to_i)
+
+puts "Part 1: "
+puts Time.now
+puts answer(players, points)
+puts Time.now
+
+puts "Part 2: "
+puts answer(players, points * 100)
