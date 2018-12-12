463f820793c98047ba0c9ce6c82c2806cc429cd1
Logan McDonald <loganmcdona11@gmail.com>
2018-12-12 17:18:57 -0500

    wip

--- 2018/day-12/solution.rb
+++ 2018/day-12/solution.rb
@@ -1,40 +1,34 @@
 # rules_input = File.open('input.txt').readlines
 rules_input = File.open('test-input.txt').readlines
 
-# initial_state = "##.#....#..#......#..######..#.####.....#......##.##.##...#..#....#.#.##..##.##.#.#..#.#....#.#..#.#"
-initial_state = "#..#.#..##......###...###"
 
-TRANSFORMATION_TIMES = 20
+def match_rule(rules, string)
+    return rules[string]
+end
+
+def transform_garden(rules, garden, transformation_times)
+    transformation_times.times do
+        expanded_garden = garden.unshift('.').unshift('.').push('.').push('.')
+        garden.each_with_index do |plot, idx|
+            if match_rule(rules, expanded_garden.slice(idx, 5).join)
+                garden[idx] = match_rule(rules, expanded_garden.slice(idx, 5).join)
+            else
+                garden[idx] = '.'
+            end
+        end
+    end
+end
 
 rules = Hash.new()
-
 rules_input.each do |line|
     plot_pattern, result = line.split(' => ').map(&:chomp)
     rules[plot_pattern] = result
 end
 
-current_generation = {}
-initial_state.chars.each_with_index{ |p, i| current_generation[i] = p }
-
-state_tracker = {
-    :current_generation => current_generation,
-    :next_generation => {}
-}
-
-def transform_garden(state_tracker, rules)
-    current_state = state_tracker[:current_generation].values.join
-    state_tracker[:current_generation].each do |k, v|
-
-    end
-    return state_tracker
-end
-
-# garden = '.' * initial_state.length + initial_state + '.' * (2*initial_state.length)
-# garden_array = (-initial_state.length..2*initial_state.length)
-
-TRANSFORMATION_TIMES.times do
-    state_tracker = transform_garden(state_tracker, rules)
-end
+# initial_state = "##.#....#..#......#..######..#.####.....#......##.##.##...#..#....#.#.##..##.##.#.#..#.#....#.#..#.#"
+initial_state = "#..#.#..##......###...###"
+garden = initial_state.chars
+garden = transform_garden(rules, garden, 3)
 
 puts "Part 1:"
-puts state_tracker[:current_generation].values.select{ |v| v == '#' }.count
+puts garden.select{ |v| v == '#' }.count
