23bc9b82e59d1ecada85239dc25332a856601536
Logan McDonald <loganmcdona11@gmail.com>
2018-12-12 15:47:31 -0500

    2018/day-1: fixes for day 1

--- 2018/day-1/solution.rb
+++ 2018/day-1/solution.rb
@@ -3,17 +3,16 @@ puts File.open("./input.txt").readlines.map(&:to_i).inject(&:+)
 
 puts "Part 2:"
 require 'set'
-changes, current_freq, freq_tracker, found = File.open("./input.txt").readlines, 0, Set.new([current_freq]), nil
+changes, current_freq, freq_tracker, found = File.open("./input.txt").readlines, 0, Set.new([current_freq]), false
 
-until found
+while !found
     for c in changes
         current_freq += c.to_i
-        if freq_tracker[current_freq]
-            found = current_freq
+        if freq_tracker.include?(current_freq)
+            found = true
+            puts current_freq
             break
         end
         freq_tracker.add(current_freq)
     end
 end
-
-puts found
