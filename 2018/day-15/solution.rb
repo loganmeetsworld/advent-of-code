eccb360c20246493a3a94d00e29aa242d5caf8ae
Logan McDonald <loganmcdona11@gmail.com>
2018-12-18 15:03:54 -0500

    2018/day-15: test input works, bug remains

--- 2018/day-15/solution.rb
+++ 2018/day-15/solution.rb
@@ -1,76 +1,129 @@
+require "set"
 
-# warrior class with attack methods, other classes inherit from it
-
-class Battlefield
-  attr_accessor :grid
-  def initialize
-    @grid = create_grid(input)
+# Two main classes: a battle and warriors. A Battle has many warriors, elves and goblins. They fight over rounds.
+class Battle
+  attr_accessor :field, :rounds, :initial_warrior_positions, :walls, :warriors
+  def initialize(input)
+    @field = input
+    @rounds = 0
+    @initial_warrior_positions = build_warriors(@field)
+    @warriors = []
   end
 
-  def create_grid(input)
-  end
-end
-
-class Round
-  def initialize(warriors, battlefield)
-    @warriors = warriors
-    @battlefield = battlefield
+  def print_battle_state()
+    puts @field
+    puts
   end
 
-  def print_battle_state
+  def warriors_on_one_side_all_dead?()
+    goblins = @warriors.select{ |w| w.type == 'G' }
+    elves = @warriors.select{ |w| w.type == 'E' }
+    return true if elves.empty?
+    return true if goblins.empty?
+  end
+
+  # We start with the warriors in their positions from our input, but these positions will change.
+  def build_warriors(field)
+    warriors = []
+    field.chars.each_with_index do |c, i|
+      if c.match(/[A-Z]/)
+        warriors.push({:type => c, :position => i})
+      end
+    end
+    return warriors
   end
 end
 
 class Warrior
-  attr_accessor :position
-  def initialize(position)
+  attr_accessor :position, :attack_power, :hit_points, :type, :battle, :turn_position
+  def initialize(type, position, turn_position, battle)
+    @battle = battle
     @position = position
-    @turn = false
+    @type = type
+    @turn_position = turn_position
     @attack_power = 3
     @hit_points = 200
   end
 
   def move()
-    if identify_targets()
-      targets = identify_targets()
-    end
-    if can_attack_closest_enemy?()
-      attack()
+    targets = @battle.warriors.reject { |t| t.type == @type }
+    in_range_targets = targets.map{ |t| find_surrounding(t.position).select{ |i| @battle.field[i] == "." } }.flatten.uniq
+    possible_moves = in_range_targets.map{ |t| find_next_move(t) }.compact.sort
+    return false if possible_moves.empty?
+    next_position = possible_moves.first.last
+
+    @battle.field[@position] = "."
+    @battle.field[next_position] = @type
+    @position = next_position
+  end
+
+  def find_surrounding(position)
+    [position - (@battle.field.split.first.length + 1), position - 1, position + 1, position +  (@battle.field.split.first.length + 1)]
+  end
+
+  def find_next_move(target)
+    possible_moves = find_surrounding(@position).select{ |i| @battle.field[i] == "." }.to_set
+    options, next_options, seen = [target], [], Set.new
+
+    distance, step = 1, nil
+    loop do
+      if options.empty?
+        break if next_options.empty?
+
+        options = next_options.sort.uniq.reverse
+        next_options = []
+        distance += 1
+      end
+
+      position = options.pop
+      if possible_moves.include?(position)
+        step = position
+        break
+      end
+
+      seen.add(position)
+      find_surrounding(position).each do |i|
+        next_options.push(i) if @battle.field[i] == "." && !seen.include?(i)
+      end
     end
 
-    square = find_nearest_adjacent_in_range_reachable_square()
-    self.position = find_shortest_path_to_square()
+    return [distance, target, step] if step
   end
 
   def attack()
-  end
-
-  def find_adjacent_in_range_squares()
-  end
-
-  def find_reachable_squares()
-  end
-
-  def find_nearest_adjacent_in_range_reachable_square()
-    # find the nearest square that is first in reading order
-    if squares.length == 1
-      square
-    else
-      break_reading_order_tie()
+    surrounding_squares = find_surrounding(@position)
+    target = @battle.warriors.select{ |w| w.type != @type && surrounding_squares.include?(w.position) }.min_by { |w| [w.hit_points, w.position] }
+    return false unless target
+    target.hit_points -= @attack_power
+    if target.hit_points <= 0
+      puts "Target has fallen! Team #{target.type} is minus one member."
+      @battle.field[target.position] = '.'
+      @battle.warriors.delete(target)
     end
-  end
-
-  def identify_targets()
-    targets = []
-    return targets
-  end
-
-  def can_attack_closest_enemy?()
+    return true
   end
 end
 
+# Initiate the battle with the puzzle input battlefield given
+field = File.open('test-input.txt').read.chomp
+battle = Battle.new(field)
+puts "Initial battle state:"
+battle.print_battle_state
 
-input = File.open('input.txt').readlines
+# Initiate the given warriors on the field, add them to battle.warriors, and also give a warrior the battle
+battle.initial_warrior_positions.each{ |w| battle.warriors.push(Warrior.new(w[:type], w[:position], w[:order], battle)) }
 
-battlefield = Battlefield.new(input)
-battlefield.visualize
+# Until all the warriors die on a give side, move the battle forward in rounds until a side wins.
+until battle.warriors_on_one_side_all_dead?
+  puts "Round ##{battle.rounds + 1}:"
+  battle.warriors.sort_by(&:position).each do |w|
+    w.move() && w.attack() unless w.attack()
+  end
+
+  battle.print_battle_state
+  battle.rounds += 1
+end
+
+# The outcome of the round is determined by how many rounds were fought, and how many points remain on the winning side
+winner = battle.warriors.first.type == 'E' ? "Elves" : "Goblins"
+puts "Part 1: Over the course of #{battle.rounds} rounds, #{winner} won with #{battle.warriors.map{ |w| w.hit_points}.inject(&:+)} hit points. Official mathematical outcome: #{battle.rounds * battle.warriors.map{ |w| w.hit_points }.inject(&:+)}"
