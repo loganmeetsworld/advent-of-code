84013a21fde9c773d06878e9c4f9a9249634962a
Logan McDonald <loganmcdona11@gmail.com>
2019-12-01 00:40:44 -0500

    slightly cleaner

--- 2019/day-1/solution.py
+++ 2019/day-1/solution.py
@@ -8,36 +8,31 @@ def calculate_weight(weight):
 
 
 def continuous_calc_weight(weight):
-    total_weight = 0
+    continuous_weight = 0
 
     while int(weight) > 0:
         weight = calculate_weight(weight)
         if weight < 0:
-            return total_weight
-        total_weight += weight
+            return continuous_weight
+        continuous_weight += weight
     
-    return total_weight
+    return continuous_weight
 
 
-def day_1(problem_input):
+def answer(problem_input, part):
     total_weight = 0
 
-    for weight in problem_input:
-        total_weight += calculate_weight(weight)
-    
-    return total_weight
-
-
-def day_2(problem_input):
-    total_weight = 0
-
-    for weight in problem_input:
-        total_weight += continuous_calc_weight(weight)
+    if part == 1:
+        for weight in problem_input:
+            total_weight += calculate_weight(weight)
+    else:
+        for weight in problem_input:
+            total_weight += continuous_calc_weight(weight)
 
     return total_weight
 
 
 problem_path = os.path.abspath(__file__)
 problem_input = problem_input(problem_path).splitlines()
-submit(problem_path, "1", day_1(problem_input))
-submit(problem_path, "2", day_2(problem_input))
+submit(problem_path, 1, answer(problem_input, 1))
+submit(problem_path, 2, answer(problem_input, 2))
