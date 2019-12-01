14b3634273668401d4b3232175e383fbfca6d142
Logan McDonald <loganmcdona11@gmail.com>
2019-12-01 14:29:29 -0500

    setup is working

---
+++ 2019/day-3/solution.py
@@ -0,0 +1,16 @@
+from aoc_utils import aoc_utils
+import os
+
+
+def answer(problem_input, part):
+    print(problem_input)
+
+
+test_cases = [
+    {'level': 1, 'input': '', 'output': ''},
+    {'level': 1, 'input': '', 'output': ''},
+    {'level': 1, 'input': '', 'output': ''},
+]
+
+problem_input = aoc_utils.fetch_and_save()
+aoc_utils.test_and_submit(test_cases, problem_input, answer)
