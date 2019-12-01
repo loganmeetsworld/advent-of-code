5ece0ac7683926f71a5def43f774c5319f0c33a3
Logan McDonald <loganmcdona11@gmail.com>
2019-12-01 00:33:31 -0500

    basic setup for day-2

---
+++ 2019/day-2/solution.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append('../../')
+from aoc_automation import problem_input, submit
+import os
+
+problem_path = os.path.abspath(__file__)
+problem_input = problem_input(problem_path)
+
+# submit(problem_path, "1")
+# submit(problem_path, "2")
