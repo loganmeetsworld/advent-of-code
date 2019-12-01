0e6f1ca447a6e6b0e8db5b8d3a3341bbf90a0799
Logan McDonald <loganmcdona11@gmail.com>
2019-11-30 23:48:43 -0500

    more setup

--- 2019/day-1/solution.py
+++ 2019/day-1/solution.py
@@ -3,16 +3,24 @@ sys.path.append('../../')
 from aoc_automation import problem_input, submit
 import os
 
+def process_input(input):
+  return input
+
 part_1_tests_passed = False
 part_2_tests_passed = False
 problem_path = os.path.abspath(__file__)
 problem_input = problem_input(problem_path)
 
-answer_1 = ''
-answer_2 = ''
+
 
 if part_1_tests_passed:
-  submit(problem_path, "1", answer_1)
+    submit(problem_path, "1", answer_1)
 
 if part_2_tests_passed:
-  submit(problem_path, "2", answer_2)
+    submit(problem_path, "2", answer_2)
+
+
+def all_pass(tests):
+    if tests[0] == False:
+        return False
+    return all(x == tests[0] for x in tests)
