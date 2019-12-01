608732c0e48cb386eb0fb1ef79d1cd9f51bb840b
Logan McDonald <loganmcdona11@gmail.com>
2019-12-01 15:22:44 -0500

    make the wording for day 1 make more sense

--- 2019/day-1/solution.py
+++ 2019/day-1/solution.py
@@ -2,30 +2,29 @@ from aoc_utils import aoc_utils
 import os
 
 
-def calculate_weight(weight):
-    return int(int(weight) / 3) - 2
+def determine_fuel(mass):
+    return int(int(mass) / 3) - 2
 
-def continuous_calc_weight(weight):
-    continuous_weight = 0
+def determine_fuel_amended(mass):
+    continuous_mass = 0
 
     while True:
-        weight = calculate_weight(weight)
-        if weight < 0:
-            return continuous_weight
-        continuous_weight += weight
+        mass = determine_fuel(mass)
+        if mass < 0:
+            return continuous_mass
+        continuous_mass += mass
 
 
 def answer(problem_input, level):
-    problem_input = problem_input.splitlines()
-    total_weight = 0
+    total_fuel_requirement = 0
 
-    for weight in problem_input:
+    for mass in problem_input.splitlines():
         if level == 1:
-            total_weight += calculate_weight(weight)
-        else:
-            total_weight += continuous_calc_weight(weight)
+            total_fuel_requirement += determine_fuel(mass)
+        elif level == 2:
+            total_fuel_requirement += determine_fuel_amended(mass)
 
-    return total_weight
+    return total_fuel_requirement
 
 
 test_cases = [
