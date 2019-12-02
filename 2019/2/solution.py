cf5e315d2555a18033be7a8c74607699689d83e4
Logan McDonald <loganmcdona11@gmail.com>
2019-12-02 08:16:11 -0500

    flimsy day 2 part 1

--- 2019/2/solution.py
+++ 2019/2/solution.py
@@ -1,9 +1,50 @@
 from aoc_utils import aoc_utils
-# from tests import cases
+from tests import cases
+
+
+def mutate_ints(integers, opscode, actionable_ints, new_place):
+    if opscode == 1:
+        new_value = integers[actionable_ints[0]] + integers[actionable_ints[1]]
+    elif opscode == 2:
+        new_value = integers[actionable_ints[0]] * integers[actionable_ints[1]]
+
+    integers[new_place] = new_value
+
+    return integers
+
+
+def restore_state(integers):
+    integers[1] = 12
+    integers[2] = 2
+
+    return integers
 
 
 def answer(problem_input, part):
-    return 0
+    integers = [int(i) for i in problem_input.split(',')]
+    integers = restore_state(integers)
+
+    opscode_pos = 0
+    opscode = integers[opscode_pos]
+    actionable_ints_min = opscode
+    actionable_ints_max = opscode + 2
+    new_place_pos = opscode_pos + 3
+    actionable_ints = integers[actionable_ints_min:actionable_ints_max]
+    new_place = integers[new_place_pos]
+
+    while True:
+        integers = mutate_ints(integers, opscode, actionable_ints, new_place)
+        opscode_pos += 4
+        opscode = integers[opscode_pos]
+        if opscode == 99:
+            return integers[0]
+        actionable_ints_min = opscode_pos + 1
+        actionable_ints_max = opscode_pos + 3
+        new_place_pos = opscode_pos + 3
+        actionable_ints = integers[actionable_ints_min:actionable_ints_max]
+        new_place = integers[new_place_pos]
+
+    return integers[0]
 
 
 aoc_utils.run(answer)
--- 2019/2/tests.py
+++ 2019/2/tests.py
@@ -1,3 +1,7 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': '1,9,10,3,2,3,11,0,99,30,40,50', 'output': '3500'},
+    {'level': 1, 'input': '1,0,0,0,99', 'output': '2'},
+    {'level': 1, 'input': '2,3,0,3,99', 'output': '2'},
+    {'level': 1, 'input': '2,4,4,5,99,0', 'output': '2'},
+    {'level': 1, 'input': '1,1,1,4,99,5,6,0,99', 'output': '30'},
 ]
