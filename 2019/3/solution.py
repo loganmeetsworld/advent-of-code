e114f5f04ff6e58914002e9aea6d356b5b76b750
Logan McDonald <loganmcdona11@gmail.com>
2019-12-03 21:47:25 -0500

    day 3 part 2

--- 2019/3/solution.py
+++ 2019/3/solution.py
@@ -7,7 +7,6 @@ from tests import cases
 def travel(wire):
     locations_traveled = []
     location = [0, 0]
-
     for pos in wire:
         direction, distance = pos[0], int(pos[1:])
         for _ in range(distance):
@@ -19,25 +18,21 @@ def travel(wire):
                 location[1] += 1
             elif direction == 'D':
                 location[1] -= 1
-
             locations_traveled.append([location[0], location[1]])
-
     return locations_traveled
 
 
-def find_shortest_distance_to_intersection(l1, l2):
-    intersections = set([str(i) for i in l1]).intersection([str(i) for i in l2])
-    intersections = [json.loads(i) for i in intersections]
-    shortest_distance = min(sum(i) for i in intersections)
-    steps = [l1.index(p) + l2.index(p) for p in intersections]
-
-    return [shortest_distance, sum(steps)]
+def find_shortest_distance_to_intersection(locations_1, locations_2):
+    intersections = [json.loads(j) for j in set([str(i) for i in locations_1]).intersection([str(i) for i in locations_2])]
+    shortest_distance = min(abs(i[0]) + abs(i[1]) for i in intersections)
+    steps = min([locations_1.index(i) + locations_2.index(i) for i in intersections])
+    return [shortest_distance, steps + 2]
 
 
 def answer(problem_input, level, test=False):
     wire_1, wire_2 = problem_input.split("\n")
-    l1, l2 = travel(wire_1.split(',')), travel(wire_2.split(','))
-    distance, steps = find_shortest_distance_to_intersection(l1, l2)
+    locations_1, locations_2 = travel(wire_1.split(',')), travel(wire_2.split(','))
+    distance, steps = find_shortest_distance_to_intersection(locations_1, locations_2)
     return distance if level == 1 else steps
 
 
