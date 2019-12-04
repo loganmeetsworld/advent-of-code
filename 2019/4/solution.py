1d64421d07e7d979b75e0c5b9b06766e358b8359
Logan McDonald <loganmcdona11@gmail.com>
2019-12-04 08:58:19 -0500

    part 2

--- 2019/4/solution.py
+++ 2019/4/solution.py
@@ -4,8 +4,10 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
-def has_double(i):
+def has_repeats(i, level):
     found = [match[0] for match in re.findall(r'((\w)\2{1,})', str(i))]
+    if level == 2:
+        found = [f for f in found if len(f) == 2]
     return len(found) > 0
 
 
@@ -13,19 +15,17 @@ def ascends_only(i):
     return int(''.join(sorted(str(i)))) == i
 
 
-def check_criteria(i):
-    return has_double(i) and ascends_only(i)
+def check_criteria(i, level):
+    return has_repeats(i, level) and ascends_only(i)
 
 
 def answer(problem_input, level, test=False):
     if test:
-        return check_criteria(problem_input)
-
+        return check_criteria(problem_input, level)
     start, end = problem_input.split('-')
     meets_criteria = []
     for i in range(int(start), int(end)):
-        meets_criteria.append(check_criteria(i))
-
+        meets_criteria.append(check_criteria(i, level))
     return meets_criteria.count(True)
 
 
--- 2019/4/tests.py
+++ 2019/4/tests.py
@@ -1,5 +1,8 @@
 cases = [
     {'level': 1, 'input': 111111, 'output': True},
     {'level': 1, 'input': 223450, 'output': False},
-    {'level': 1, 'input': 123789, 'output': False}
+    {'level': 1, 'input': 123789, 'output': False},
+    {'level': 2, 'input': 112233, 'output': True},
+    {'level': 2, 'input': 123444, 'output': False},
+    {'level': 2, 'input': 111122, 'output': True}
 ]
