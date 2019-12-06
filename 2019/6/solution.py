5b3fccea6af510ae308a28894d2c47e0a9b9114b
Logan McDonald <loganmcdona11@gmail.com>
2019-12-06 11:35:56 -0500

    fixups

--- 2019/6/solution.py
+++ 2019/6/solution.py
@@ -4,21 +4,18 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
+def orbit_count(edges):
+    graph = nx.Graph()
+    [graph.add_edge(edge[0], edge[1]) for edge in edges]
+    total = 0
+    for node in graph.nodes: total += len(nx.descendants(graph, node))
+    return total
+
+
 def shortest_path(edges):
     graph = nx.Graph()
     [graph.add_edge(edge[0], edge[1]) for edge in edges]
-    path_len = nx.shortest_path_length(graph, 'YOU', 'SAN')
-    return path_len - 2
-
-
-def orbit_count(edges):
-    graph = nx.DiGraph()
-    [graph.add_edge(edge[0], edge[1]) for edge in edges]
-    total = 0
-    for node in graph.nodes:
-        total += len(nx.descendants(graph, node))
-
-    return total
+    return nx.shortest_path_length(graph, 'YOU', 'SAN') - 2
 
 
 def answer(problem_input, level, test=False):
