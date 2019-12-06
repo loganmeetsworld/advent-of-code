360e9ea5c3f5d045dcb2750c5fe96b398119fae7
Logan McDonald <loganmcdona11@gmail.com>
2019-12-06 08:49:55 -0500

    wip

--- 2019/6/solution.py
+++ 2019/6/solution.py
@@ -2,17 +2,25 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
-def orbit_count(orbits):
-    for orbit in orbits:
-        orbitted, orbitting = orbit.split(')')
-        # MAKE A TREE
+def create_tree(edges):
+    tree = {}
+    for child, parent in edges:
+        tree.setdefault(parent, []).append(child)
+        tree.setdefault(child, []).append(parent)
+    
+    return tree
 
+
+def orbit_count(edges):
+    tree = create_tree(edges)
+    print(tree)
+  
     return 1
 
 
 def answer(problem_input, level, test=False):
-    orbits = problem_input.splitlines()
-    return orbit_count(orbits)
+    edges = problem_input.splitlines()
+    return orbit_count([e.split(')') for e in edges])
 
 
 aoc_utils.run(answer, cases)
