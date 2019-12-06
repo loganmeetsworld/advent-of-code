7782b194d6e6c324d0704b1c4c228ebae7db62f5
Logan McDonald <loganmcdona11@gmail.com>
2019-12-06 08:25:05 -0500

    general strat

--- 2019/6/solution.py
+++ 2019/6/solution.py
@@ -2,8 +2,17 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
+def orbit_count(orbits):
+    for orbit in orbits:
+        orbitted, orbitting = orbit.split(')')
+        # MAKE A TREE
+
+    return 1
+
+
 def answer(problem_input, level, test=False):
-    return 0
+    orbits = problem_input.splitlines()
+    return orbit_count(orbits)
 
 
 aoc_utils.run(answer, cases)
