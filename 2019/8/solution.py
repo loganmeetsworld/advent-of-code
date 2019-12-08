fca3910451b4e3fb034e0fc1a6fbf808ef859b0c
Logan McDonald <loganmcdona11@gmail.com>
2019-12-08 00:40:15 -0500

    day 8 part 1 and setup for part 2

--- 2019/8/solution.py
+++ 2019/8/solution.py
@@ -1,9 +1,32 @@
+from collections import Counter
+
 from aoc_utils import aoc_utils
 from tests import cases
 
+WIDTH = 25
+HEIGHT = 6
+
+def chunks(string, n):
+    for i in range(0, len(string), n): yield string[i:i + n]
+
+
+def count_least_zeros(layers):
+    counters = [Counter(l) for l in layers]
+    least_zero = [c for c in counters if c['0'] == min(c['0'] for c in counters)][0]
+    
+    return least_zero['1'] * least_zero['2']
+
+
+def draw(picture):
+    # TODO: do this part
+    return 0
+
 
 def answer(problem_input, level, test=False):
-    return 0
+    layers = list(chunks(problem_input, WIDTH * HEIGHT))
+    if level == 1: return count_least_zeros(layers)
+    elif level == 2: return draw(problem_input)
+
 
 
 aoc_utils.run(answer, cases)
--- 2019/8/tests.py
+++ 2019/8/tests.py
@@ -1,3 +1,4 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': '123456789012', 'output': 4},
+    {'level': 2, 'input': '0222112222120000', 'output': '01\n10'}
 ]
