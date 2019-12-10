4b354d20279ddfeb3268edf2979d1d06822221a4
Logan McDonald <loganmcdona11@gmail.com>
2019-12-10 13:18:56 -0500

    day 10 part 1, truly unsustainable for part 2

--- 2019/10/solution.py
+++ 2019/10/solution.py
@@ -2,13 +2,38 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
-def find_max_viewed(asteroid_map):
+def find_asteroids(asteroid_map):
     views = []
-    for line in asteroid_map.splitlines():
-        view = line.count('#')
-        views.append(view)
+    for idx_y, y in enumerate(asteroid_map.splitlines()):
+        for idx_x, x in enumerate(list(y)):
+            views.append({'text': x, 'x': idx_x, 'y': idx_y})
+    asteroids = [v for v in views if v['text'] == '#']
+    return asteroids
 
-    return max(views)
+
+def sight_line(x_asteroid, y_asteroid, x, y):
+    if x_asteroid == x:
+        force = '+' if y_asteroid < y else '-'
+    else:
+        force = '+' if x_asteroid < x else '-'
+    try:
+        slope = (y_asteroid - y) / (x_asteroid - x)
+        return str(slope) + force
+    except ZeroDivisionError:
+        return f"inf-{force}"
+
+
+def find_max_viewed(asteroid_map):
+    asteroids = find_asteroids(asteroid_map)
+    targets = []
+
+    for a in asteroids:
+        asteroid_set = set()
+        for b in asteroids:
+            asteroid_set.add(sight_line(a['x'], a['y'], b['x'], b['y']))
+        targets.append(len(asteroid_set))
+
+    return max(targets)
 
 
 def answer(problem_input, level, test=False):
