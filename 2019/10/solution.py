61b4d3ab454f3b50d43f6faa8d667dc6dff01de5
Logan McDonald <loganmcdona11@gmail.com>
2019-12-10 06:52:05 -0500

    setup day and tests

--- 2019/10/solution.py
+++ 2019/10/solution.py
@@ -2,8 +2,18 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
+def find_max_viewed(asteroid_map):
+    views = []
+    for line in asteroid_map.splitlines():
+        view = line.count('#')
+        views.append(view)
+
+    return max(views)
+
+
 def answer(problem_input, level, test=False):
-    return 0
+    max_viewed = find_max_viewed(problem_input)
+    return max_viewed
 
 
 aoc_utils.run(answer, cases)
