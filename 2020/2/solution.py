06291c94e8d9245bb0123dd77c62d4e77e17b621
Logan McDonald <loganmcdona11@gmail.com>
2020-12-02 08:52:05 -0500

    2020/2: complete p1 and 2

--- 2020/2/solution.py
+++ 2020/2/solution.py
@@ -1,9 +1,24 @@
+import re
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
+def validate_rule(rule, level):
+    mn, mx, letter, password = list(re.findall(r"(\d+)-(\d+) (\w): (\w+)", rule)[0])
+    if level == 1:
+        return password.count(letter) >= int(mn) and password.count(letter) <= int(mx)
+    elif level == 2:
+        return (password[int(mn) - 1] == letter or password[int(mx) - 1] == letter) and not (password[int(mn) - 1] == letter and password[int(mx) - 1] == letter)
+
+
 def answer(problem_input, level, test=False):
-    return 0
+    valid_pw_count = 0
+    password_rules = problem_input.splitlines()
+    for password_rule in password_rules:
+        if validate_rule(password_rule, level):
+            valid_pw_count += 1
+    return valid_pw_count
 
 
 aoc_utils.run(answer, cases)
--- 2020/2/tests.py
+++ 2020/2/tests.py
@@ -1,3 +1,4 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': '1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc', 'output': 2},
+    {'level': 2, 'input': '1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc', 'output': 1},
 ]
