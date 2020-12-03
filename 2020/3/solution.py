513e982ab201ca991d76a37a45adf7483f94cf1d
Logan McDonald <loganmcdona11@gmail.com>
2020-12-03 08:54:07 -0500

    2020/3: rest of day 3 submitted

--- 2020/3/solution.py
+++ 2020/3/solution.py
@@ -4,22 +4,26 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
-def traverse(rows, x, y):
-    tree_pos, tree_count = 0, 0
-    for row in rows:
-        if row[tree_pos % len(row)] == '#':
-            tree_count += 1
-        tree_pos += y
-    print(tree_count)
-    return tree_count
+def traverse(problem_input, slopes):
+    rows = problem_input.splitlines()
+    tree_counts = []
+    for slope in slopes:
+        tree_pos, tree_count, row_pos = 0, 0, 0
+        while row_pos < len(rows):
+            row = rows[row_pos]
+            if row[tree_pos % len(row)] == '#':
+                tree_count += 1
+            tree_pos += slope[1]
+            row_pos += slope[0]
+        tree_counts.append(tree_count)
+    return prod(tree_counts)
 
 
 def answer(problem_input, level, test=False):
-    rows = problem_input.splitlines()
     if level == 1:
-        return traverse(rows, 1, 3)
+        return traverse(problem_input, [[1, 3]])
     elif level == 2:
-        return prod([traverse(rows, 1, 1), traverse(rows, 1, 3), traverse(rows, 1, 5), traverse(rows, 1, 7), traverse(rows, 2, 1)])
+        return traverse(problem_input, [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]])
 
 
 aoc_utils.run(answer, cases)
