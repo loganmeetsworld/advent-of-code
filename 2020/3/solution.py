bca09434a74320c266052233f93fbafad60d414d
Logan McDonald <loganmcdona11@gmail.com>
2020-12-03 09:38:37 -0500

    use agatas terrain translator

--- 2020/3/solution.py
+++ 2020/3/solution.py
@@ -5,13 +5,12 @@ from tests import cases
 
 
 def traverse(rows, slopes):
-    tree_counts = []
+    terrain_translation, tree_counts = {'.': 0, '#': 1}, []
     for slope in slopes:
         tree_pos, row_pos, tree_count = 0, 0, 0
         while row_pos < len(rows):
             row = rows[row_pos]
-            if rows[row_pos][tree_pos % len(row)] == '#':
-                tree_count += 1
+            tree_count += terrain_translation[row[tree_pos % len(row)]]
             tree_pos += slope[0]
             row_pos += slope[1]
         tree_counts.append(tree_count)
@@ -19,11 +18,10 @@ def traverse(rows, slopes):
 
 
 def answer(problem_input, level, test=False):
-    rows = problem_input.splitlines()
     slopes = [[3, 1]]
     if level == 2:
         slopes += [[1, 1], [5, 1], [7, 1], [1, 2]]
-    return traverse(rows, slopes)
+    return traverse(problem_input.splitlines(), slopes)
 
 
 aoc_utils.run(answer, cases)
