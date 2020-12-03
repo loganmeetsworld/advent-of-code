2f725c4252af7320fe3a474716cc59e02d334090
Logan McDonald <loganmcdona11@gmail.com>
2020-12-03 08:57:54 -0500

    change slope to be correct

--- 2020/3/solution.py
+++ 2020/3/solution.py
@@ -13,17 +13,17 @@ def traverse(problem_input, slopes):
             row = rows[row_pos]
             if row[tree_pos % len(row)] == '#':
                 tree_count += 1
-            tree_pos += slope[1]
-            row_pos += slope[0]
+            tree_pos += slope[0]
+            row_pos += slope[1]
         tree_counts.append(tree_count)
     return prod(tree_counts)
 
 
 def answer(problem_input, level, test=False):
     if level == 1:
-        return traverse(problem_input, [[1, 3]])
+        return traverse(problem_input, [[3, 1]])
     elif level == 2:
-        return traverse(problem_input, [[1, 1], [1, 3], [1, 5], [1, 7], [2, 1]])
+        return traverse(problem_input, [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]])
 
 
 aoc_utils.run(answer, cases)
