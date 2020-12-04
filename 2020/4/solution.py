1f1e4e9edc5a7b7edd4dd832cc8fe6f7f74647f2
Logan McDonald <loganmcdona11@gmail.com>
2020-12-04 09:29:29 -0500

    2020/4: partway part 2

--- 2020/4/solution.py
+++ 2020/4/solution.py
@@ -2,17 +2,51 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 REQUIRED_ATTRIBUTES = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-# OPTIONAL_ATTRIBUTES = ["cid"]
 
+
+def validate_attrs(atts):
+    return [a for a in atts if len(list(set(REQUIRED_ATTRIBUTES) - set(a.keys()))) == 0]
+
+# byr (Birth Year) - four digits; at least 1920 and at most 2002.
+# iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+# eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+# hgt (Height) - a number followed by either cm or in:
+
+# If cm, the number must be at least 150 and at most 193.
+# If in, the number must be at least 59 and at most 76.
+
+
+# hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+# ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+# pid (Passport ID) - a nine-digit number, including leading zeroes.
+# cid (Country ID) - ignored, missing or not.
+
+def validate_passport(e):
+    if int(e['byr']) < 1920 or > 2002:
+        return False
+
+    if int(e['iyr']) < 2010 or > 2020:
+        return False
+
+    if int(e['eyr']) < 2020 or > 2030:
+        return False
+
+    return True
 
 def answer(problem_input, level, test=False):
-    valid_count = 0
-    elves = [e.replace("\n", " ") for e in problem_input.split("\n\n")]
-    for e in elves:
-        attributes = [a.split(':')[0] for a in e.split(' ')]
-        if len(list(set(REQUIRED_ATTRIBUTES) - set(attributes))) == 0:
-            valid_count += 1
-    return valid_count
+    elves = []
+    for e in problem_input.split("\n\n"):
+        edata = {}
+        for attribute in e.replace("\n", " ").split(" "):
+            edata[attribute.split(':')[0]] = attribute.split(':')[1]
+        elves.append(edata)
+
+    complete_passports = validate_attrs(elves)
+    if level == 1:
+        return len(complete_passports)
+    else:
+        valid_passports = [e for e in complete_passports if validate_passport(e)]
+        return len(valid_passports)
 
 
 aoc_utils.run(answer, cases)
--- 2020/4/tests.py
+++ 2020/4/tests.py
@@ -1,3 +1,3 @@
 cases = [
-    {'level': 1, 'input': "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm\n\niyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929\n\nhcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm\n\nhcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in\n", 'output': 2},
+    {'level': 1, 'input': "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\nbyr:1937 iyr:2017 cid:147 hgt:183cm\n\niyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\nhcl:#cfa07d byr:1929\n\nhcl:#ae17e1 iyr:2013\neyr:2024\necl:brn pid:760753108 byr:1931\nhgt:179cm\n\nhcl:#cfa07d eyr:2025 pid:166559648\niyr:2011 ecl:brn hgt:59in", 'output': 2},
 ]
