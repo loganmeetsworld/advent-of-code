b9d632eeb8a6b622bbdeac31fdae78fd12ffd772
Logan McDonald <loganmcdona11@gmail.com>
2020-12-05 13:22:26 -0500

    2020/5: final

--- 2020/5/solution.py
+++ 2020/5/solution.py
@@ -5,11 +5,11 @@ from tests import cases
 def bsp(code, arrow, max_range):
     r = [0, max_range]
     for letter in code:
-        range_length = (r[1] - r[0] + 1) / 2
+        rlength = (r[1] - r[0] + 1) / 2
         if letter == arrow:
-            r[0] += range_length
+            r[0] += rlength
         else:
-            r[1] -= range_length
+            r[1] -= rlength
 
     return int(r[0])
 
@@ -17,9 +17,8 @@ def bsp(code, arrow, max_range):
 def answer(problem_input, level, test=False):
     seat_ids = []
     for boarding_pass in problem_input.splitlines():
-        row = bsp(boarding_pass[:7], 'B', 127)
-        column = bsp(boarding_pass[7:], 'R', 7)
-        seat_ids.append(row * 8 + column)
+        row, col = bsp(boarding_pass[:7], 'B', 127), bsp(boarding_pass[7:], 'R', 7)
+        seat_ids.append(row * 8 + col)
 
     max_seat_id, min_seat_id = max(seat_ids),  min(seat_ids)
     return max_seat_id if level == 1 else set(range(min_seat_id, max_seat_id)) - set(seat_ids)
