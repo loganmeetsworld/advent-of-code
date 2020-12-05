f372e220904432c1e748b6c80cd08adb80fae450
Logan McDonald <loganmcdona11@gmail.com>
2020-12-05 18:59:00 -0500

    2020/5: why not put it all on one line

--- 2020/5/solution.py
+++ 2020/5/solution.py
@@ -2,12 +2,8 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
-def binary_string_to_decimal(bstring):
-    return int(bstring.translate(str.maketrans("FBLR", "0101")), 2)
-
-
 def answer(problem_input, level, test=False):
-    seat_ids = sorted([binary_string_to_decimal(boarding_pass) for boarding_pass in problem_input.splitlines()])
+    seat_ids = sorted([int(boarding_pass.translate(str.maketrans("FBLR", "0101")), 2) for boarding_pass in problem_input.splitlines()])
     return seat_ids[-1] if level == 1 else (set(range(seat_ids[0], seat_ids[-1])) - set(seat_ids)).pop()
 
 
