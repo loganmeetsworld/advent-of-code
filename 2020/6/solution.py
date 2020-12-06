e8cc43bf7ca62ccb395d14aaa45e444af4880cab
Logan McDonald <loganmcdona11@gmail.com>
2020-12-06 08:44:01 -0500

    2020/6: basic solution works

--- 2020/6/solution.py
+++ 2020/6/solution.py
@@ -1,10 +1,24 @@
+from collections import Counter
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
 def answer(problem_input, level, test=False):
-    counts = [len(set(group.replace('\n', ''))) for group in problem_input.split('\n\n')]
-    return sum(counts)
+    if level == 1:
+        counts = [len(set(group.replace('\n', ''))) for group in problem_input.split('\n\n')]
+        return sum(counts)
+    elif level == 2:
+        counts = 0
+        for group in problem_input.split('\n\n'):
+            counter = Counter()
+            for elf in group.splitlines():
+                counter.update(elf)
+            for k, v in counter.items():
+                if v / len(group.splitlines()) == 1:
+                    counts += 1
+
+        return counts
 
 
 aoc_utils.run(answer, cases)
--- 2020/6/tests.py
+++ 2020/6/tests.py
@@ -1,3 +1,4 @@
 cases = [
     {'level': 1, 'input': "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb", 'output': 11},
+    {'level': 2, 'input': "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb", 'output': 6},
 ]
