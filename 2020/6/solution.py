0ad9fcdfda5ff1a844bb44b040814dfacb58d4af
Logan McDonald <loganmcdona11@gmail.com>
2020-12-06 12:40:51 -0500

    2020/6: small refactor

--- 2020/6/solution.py
+++ 2020/6/solution.py
@@ -6,15 +6,13 @@ from tests import cases
 
 def answer(problem_input, level, test=False):
     if level == 1:
-        counts = [len(set(group.replace('\n', ''))) for group in problem_input.split('\n\n')]
-        return sum(counts)
+        return sum([len(set(group.replace('\n', ''))) for group in problem_input.split('\n\n')])
     elif level == 2:
         counts = 0
         for group in problem_input.split('\n\n'):
             counter = Counter()
-            for elf in group.splitlines():
-                counter.update(elf)
-            for k, v in counter.items():
+            [counter.update(elf) for elf in group.splitlines()]
+            for _, v in counter.items():
                 if v / len(group.splitlines()) == 1:
                     counts += 1
 
