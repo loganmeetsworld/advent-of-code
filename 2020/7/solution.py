652492f5f1218b8afb153d8f45fd8c659a81df36
Logan McDonald <loganmcdona11@gmail.com>
2020-12-07 09:28:34 -0500

    2020/7: close maybe

--- 2020/7/solution.py
+++ 2020/7/solution.py
@@ -5,25 +5,42 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 PARENT_BAG_REGEX = r'^(\w+ \w+)'
-CHILD_BAG_REGEX = r'\d+ (\w+ \w+)'
+BAG_COUNT_REGEX = r'(\d+) (\w+ \w+)'
 MY_BAG = 'shiny gold'
 
 
-def count_bags(bag, bag_map):
+def find_inner_bags(bag, bag_count_map):
     for inner_bag in bag:
-        bag = set(bag) | count_bags(bag_map[inner_bag], bag_map)
+        bag = set(bag) | find_inner_bags(bag_count_map[inner_bag], bag_count_map)
 
     return set(bag)
 
 
+def find_bag_mandate(bag, bag_size_map):
+    count = 0
+    for inner_bag in bag:
+        print(inner_bag)
+        print(bag_size_map)
+        count += int(inner_bag[0])
+        find_bag_mandate(bag_size_map[inner_bag[1]], bag_size_map)
+
+    return count
+
+
 def answer(problem_input, level, test=False):
-    bag_map = defaultdict(list)
+    bag_count_map = defaultdict(list)
+    bag_size_map = {}
     for line in problem_input.splitlines():
         parent_bag = re.search(PARENT_BAG_REGEX, line).group(1)
-        for bag in re.findall(CHILD_BAG_REGEX, line):
-            bag_map[bag].append(parent_bag)
+        child_bags = re.findall(BAG_COUNT_REGEX, line)
+        bag_size_map[parent_bag] = child_bags
+        for bag in child_bags:
+            bag_count_map[bag[1]].append(parent_bag)
 
-    return len(count_bags(bag_map[MY_BAG], bag_map))
+    if level == 1:
+        return len(find_inner_bags(bag_count_map[MY_BAG], bag_count_map))
+    else:
+        return find_bag_mandate(bag_size_map[MY_BAG], bag_size_map)
 
 
 aoc_utils.run(answer, cases)
