47e27bbf00b72f4c353a54f35534f91c1c9ca4f6
Logan McDonald <loganmcdona11@gmail.com>
2020-12-08 08:21:20 -0500

    2020/8: add tests for p2

--- 2020/8/tests.py
+++ 2020/8/tests.py
@@ -1,3 +1,4 @@
 cases = [
     {'level': 1, 'input': "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6", 'output': 5},
+    {'level': 2, 'input': "nop +0\nacc +1\njmp +4\nacc +3\njmp -3\nacc -99\nacc +1\njmp -4\nacc +6", 'output': 8},
 ]
