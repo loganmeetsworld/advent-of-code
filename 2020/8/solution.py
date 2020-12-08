8b8cccc6f69ddcdf3413487fd4a272fe4ff42d86
Logan McDonald <loganmcdona11@gmail.com>
2020-12-08 08:54:19 -0500

    wip p2

--- 2020/8/solution.py
+++ 2020/8/solution.py
@@ -1,3 +1,5 @@
+import copy
+
 from aoc_utils import aoc_utils
 from tests import cases
 
@@ -9,6 +11,7 @@ class Console():
         self.pointer = 0
         self.instrs = [i.split(' ') for i in program.splitlines()]
         self.travel_log = []
+        self.fixed = False
 
     def translate(self):
         self.travel_log.append(self.pointer % len(self.instrs))
@@ -26,12 +29,35 @@ class Console():
     def accumulate(self):
         while self.pointer not in self.travel_log:
             self.translate()
+            if self.pointer == len(self.instrs):
+                self.fixed = True
+                break
+
+
+def build_inputs(program):
+    inputs = []
+    translate = {'jmp': 'nop', 'nop': 'jmp'}
+    program = [i.split(' ') for i in program.splitlines()]
+    for pos, (command, value) in enumerate(program):
+        p = copy.copy(program)
+        if command == 'jmp' or command == 'nop':
+            p[pos][0] = translate[command]
+        inputs.append('\n'.join([' '.join(i) for i in p]))
+    return inputs
 
 
 def answer(problem_input, level, test=False):
-    console = Console(problem_input)
-    console.accumulate()
-    return console.accumulator
+    if level == 1:
+        console = Console(problem_input)
+        console.accumulate()
+        return console.accumulator
+    else:
+        possible_inputs = build_inputs(problem_input)
+        for i in possible_inputs:
+            console = Console(i)
+            console.accumulate()
+            if console.fixed:
+                return console.accumulator
 
 
 aoc_utils.run(answer, cases)
