e9a5c01b1773913191c2dc0c1b716f6f2f139ecd
Logan McDonald <loganmcdona11@gmail.com>
2020-12-09 09:15:20 -0500

    2020/9: part 1

--- 2020/9/solution.py
+++ 2020/9/solution.py
@@ -1,9 +1,22 @@
+import itertools
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
+def valid_check(n, seen):
+    sums = [sum(i) for i in itertools.combinations(seen, 2)]
+    return n in sums
+
+
 def answer(problem_input, level, test=False):
-    return 0
+    preamble = 5 if test else 25
+    numbers = [int(i) for i in problem_input.splitlines()]
+    seen = numbers[:preamble]
+    for n in numbers[preamble:]:
+        if not valid_check(n, seen[-preamble:]):
+            return n
+        seen.append(n)
 
 
 aoc_utils.run(answer, cases)
--- 2020/9/tests.py
+++ 2020/9/tests.py
@@ -1,3 +1,3 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': "35\n20\n15\n25\n47\n40\n62\n55\n65\n95\n102\n117\n150\n182\n127\n219\n299\n277\n309\n576", 'output': 127},
 ]
