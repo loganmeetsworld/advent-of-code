3a6416cba5a0943b8d187c66f9a537a87dd23792
Logan McDonald <loganmcdona11@gmail.com>
2020-12-09 09:31:06 -0500

    2020/9: part 2

--- 2020/9/solution.py
+++ 2020/9/solution.py
@@ -9,14 +9,26 @@ def valid_check(n, seen):
     return n in sums
 
 
+def find_encryption_weakness(invalid_number, preamble, numbers):
+    for contiguous_range in range(2, len(numbers)):
+        for number in range(len(numbers) - contiguous_range):
+            range_end = number + contiguous_range
+            if sum(numbers[number:range_end]) == invalid_number:
+                min_range = min(numbers[number:range_end])
+                max_range = max(numbers[number:range_end])
+                return min_range + max_range
+
+
 def answer(problem_input, level, test=False):
     preamble = 5 if test else 25
     numbers = [int(i) for i in problem_input.splitlines()]
     seen = numbers[:preamble]
     for n in numbers[preamble:]:
         if not valid_check(n, seen[-preamble:]):
-            return n
+            invalid_number = n
         seen.append(n)
+
+    return invalid_number if level == 1 else find_encryption_weakness(invalid_number, preamble, numbers)
 
 
 aoc_utils.run(answer, cases)
