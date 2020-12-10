7a2a9652d1a83cc62aa64e4bccbbefc4f259e8bc
Logan McDonald <loganmcdona11@gmail.com>
2020-12-10 08:36:56 -0500

    2020/10: setup tests for part 2

--- 2020/10/solution.py
+++ 2020/10/solution.py
@@ -2,23 +2,29 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
-def find_differences(numbers, delta) -> int:
-    count = 0
+def find_differences(numbers: list, delta: int) -> int:
+    differences = 0
     for pos, n in enumerate(numbers):
         if len(numbers) == pos + 1:
             continue
         if n - numbers[pos + 1] == delta:
-            count += 1
-    return count
+            differences += 1
+    return differences
+
+
+def find_arrangements(numbers: list) -> int:
+    return 0
 
 
 def answer(problem_input, level, test=False):
     numbers = sorted([int(i) for i in problem_input.splitlines()], reverse=True)
     numbers.insert(0, max(numbers) + 3)
     numbers.append(0)
-    ones = find_differences(numbers, 1)
-    threes = find_differences(numbers, 3)
-    return threes * ones
+    if level == 1:
+        ones, threes = find_differences(numbers, 1), find_differences(numbers, 3)
+        return threes * ones
+    elif level == 2:
+        return find_arrangements(numbers)
 
 
 aoc_utils.run(answer, cases)
