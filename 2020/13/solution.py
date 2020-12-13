28792e888cec359248bbcbdc33ff958f80853704
Logan McDonald <loganmcdona11@gmail.com>
2020-12-13 10:55:12 -0500

    2020/13: calculate using lcm

--- 2020/13/solution.py
+++ 2020/13/solution.py
@@ -1,7 +1,13 @@
 import copy
+from math import gcd
 
 from aoc_utils import aoc_utils
 from tests import cases
+
+
+def least_common_multiple(a, b):
+    # https://www.w3schools.com/python/ref_math_gcd.asp
+    return a * b // gcd(a, b)
 
 
 def calculate_bus_departure(earliest_departure, schedule):
@@ -15,15 +21,25 @@ def calculate_bus_departure(earliest_departure, schedule):
 
 
 def calculate_cascading_bus_minutes(schedule):
-    return 0
+    position, bus_number = schedule[0]
+    for next_position, next_bus_number in schedule[1:]:
+        while True:
+            position += bus_number
+            if (position + next_position) % next_bus_number == 0:
+                break
+
+        bus_number = least_common_multiple(bus_number, next_bus_number)
+
+    return position
 
 
 def answer(problem_input, level, test=False):
     earliest_departure, schedule = problem_input.splitlines()
-    schedule = [int(i) for i in schedule.replace('x', '').split(',') if i]
     if level == 1:
+        schedule = [int(bus) for bus in schedule.replace('x', '').split(',') if bus]
         return calculate_bus_departure(int(earliest_departure), schedule)
     else:
+        schedule = [[i, int(bus)] for i, bus in enumerate(schedule.split(',')) if bus != 'x']
         return calculate_cascading_bus_minutes(schedule)
 
 
