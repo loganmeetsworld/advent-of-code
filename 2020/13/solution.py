2c13fb57b61823428239983de817cc7d46801746
Logan McDonald <loganmcdona11@gmail.com>
2020-12-13 08:45:23 -0500

    2020/13: setup part 2 tests

--- 2020/13/solution.py
+++ 2020/13/solution.py
@@ -4,29 +4,27 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
-def bus_arriving(ed, schedule):
-    for bus in schedule:
-        if ed % bus == 0:
-            return bus
-
-
 def calculate_bus_departure(earliest_departure, schedule):
     ed = copy.copy(earliest_departure)
     while True:
         ed += 1
-        if ed == 100:
-            break
-        bus = bus_arriving(ed, schedule)
-        if bus:
-            return bus * (ed - earliest_departure)
-            break
+        for bus in schedule:
+            if ed % bus == 0:
+                return bus * (ed - earliest_departure)
+                break
+
+
+def calculate_cascading_bus_minutes(schedule):
+    return 0
 
 
 def answer(problem_input, level, test=False):
     earliest_departure, schedule = problem_input.splitlines()
     schedule = [int(i) for i in schedule.replace('x', '').split(',') if i]
-    bus_departure = calculate_bus_departure(int(earliest_departure), schedule)
-    return bus_departure
+    if level == 1:
+        return calculate_bus_departure(int(earliest_departure), schedule)
+    else:
+        return calculate_cascading_bus_minutes(schedule)
 
 
 aoc_utils.run(answer, cases)
--- 2020/13/tests.py
+++ 2020/13/tests.py
@@ -1,3 +1,8 @@
 cases = [
     {'level': 1, 'input': "939\n7,13,x,x,59,x,31,19", 'output': 295},
+    {'level': 2, 'input': "0\n17,x,13,19", 'output': 3417},
+    {'level': 2, 'input': "0\n67,7,59,61", 'output': 754018},
+    {'level': 2, 'input': "0\n67,x,7,59,61", 'output': 779210},
+    {'level': 2, 'input': "0\n67,7,x,59,61", 'output': 1261476},
+    {'level': 2, 'input': "0\n1789,37,47,1889", 'output': 1202161486},
 ]
