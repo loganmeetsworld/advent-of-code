1de0e19adb5c3d39c5f3bfca37fe8797b56753f2
Logan McDonald <loganmcdona11@gmail.com>
2020-12-12 22:36:12 -0500

    2020/12: a little more concise

--- 2020/12/solution.py
+++ 2020/12/solution.py
@@ -5,70 +5,63 @@ from tests import cases
 
 
 class Ferry():
-    def __init__(self, level):
-        self.level = level
+    def __init__(self):
         self.position = [0, 0]
-        self.waypoint = [-1, 10]
+        self.waypoint_position = [-1, 10]
         self.directions = ['E', 'S', 'W', 'N']
 
     def get_distance_traveled(self):
         return sum([abs(i) for i in self.position])
 
-    def navigate(self, action, amount):
-        if action == 'F':
-            if self.level == 1:
-                action = self.directions[0]
-            else:
-                self.position[0] += (self.waypoint[0] * amount)
-                self.position[1] += (self.waypoint[1] * amount)
+    def handle_cardinal_direction(self, action, amount, pos):
         if action == 'N':
-            if self.level == 1:
-                self.position[0] -= amount
-            else:
-                self.waypoint[0] -= amount
-        if action == 'S':
-            if self.level == 1:
-                self.position[0] += amount
-            else:
-                self.waypoint[0] += amount
-        if action == 'E':
-            if self.level == 1:
-                self.position[1] += amount
-            else:
-                self.waypoint[1] += amount
-        if action == 'W':
-            if self.level == 1:
-                self.position[1] -= amount
-            else:
-                self.waypoint[1] -= amount
+            pos[0] -= amount
+        elif action == 'S':
+            pos[0] += amount
+        elif action == 'E':
+            pos[1] += amount
+        elif action == 'W':
+            pos[1] -= amount
+
+    def navigate_ship_and_waypoint(self, action, amount):
+        if action == 'F':
+            self.position[0] += (self.waypoint_position[0] * amount)
+            self.position[1] += (self.waypoint_position[1] * amount)
+
+        if action in self.directions:
+            self.handle_cardinal_direction(action, amount, self.waypoint_position)
+        elif action == 'R':
+            if amount == 90:
+                self.waypoint_position = [self.waypoint_position[1], -self.waypoint_position[0]]
+            elif amount == 270:
+                self.waypoint_position = [-self.waypoint_position[1], self.waypoint_position[0]]
+            elif amount == 180:
+                self.waypoint_position = [-self.waypoint_position[0], -self.waypoint_position[1]]
+        elif action == 'L':
+            if amount == 90:
+                self.waypoint_position = [-self.waypoint_position[1], self.waypoint_position[0]]
+            elif amount == 270:
+                self.waypoint_position = [self.waypoint_position[1], -self.waypoint_position[0]]
+            elif amount == 180:
+                self.waypoint_position = [-self.waypoint_position[0], -self.waypoint_position[1]]
+
+    def navigate_ship(self, action, amount):
+        if action == 'F':
+            action = self.directions[0]
+        if action in self.directions:
+            self.handle_cardinal_direction(action, amount, self.position)
         if action == 'R':
-            if self.level == 1:
-                self.directions = self.directions[int(amount / 90):] + self.directions[:int(amount / 90)]
-            else:
-                if amount == 90:
-                    self.waypoint = [self.waypoint[1], -self.waypoint[0]]
-                elif amount == 270:
-                    self.waypoint = [-self.waypoint[1], self.waypoint[0]]
-                elif amount == 180:
-                    self.waypoint = [-self.waypoint[0], -self.waypoint[1]]
+            self.directions = self.directions[int(amount / 90):] + self.directions[:int(amount / 90)]
         if action == 'L':
-            if self.level == 1:
-                self.directions = self.directions[-int(amount / 90):] + self.directions[:-int(amount / 90)]
-            else:
-                if amount == 90:
-                    self.waypoint = [-self.waypoint[1], self.waypoint[0]]
-                elif amount == 270:
-                    self.waypoint = [self.waypoint[1], -self.waypoint[0]]
-                elif amount == 180:
-                    self.waypoint = [-self.waypoint[0], -self.waypoint[1]]
+            self.directions = self.directions[-int(amount / 90):] + self.directions[:-int(amount / 90)]
 
 
 def answer(problem_input, level, test=False):
-    ferry = Ferry(level)
+    ferry = Ferry()
     for move in problem_input.splitlines():
         match = re.match(r'([NSEWLRF])(\d+)', move)
         action, amount = match[1], int(match[2])
-        ferry.navigate(action, amount)
+        ferry.navigate_ship(action, amount) if level == 1 else ferry.navigate_ship_and_waypoint(action, amount)
 
     return ferry.get_distance_traveled()
 
--- 2020/12/tests.py
+++ 2020/12/tests.py
@@ -1,4 +1,4 @@
 cases = [
-    # {'level': 1, 'input': "F10\nN3\nF7\nR90\nF11", 'output': 25},
+    {'level': 1, 'input': "F10\nN3\nF7\nR90\nF11", 'output': 25},
     {'level': 2, 'input': "F10\nN3\nF7\nR90\nF11", 'output': 286},
 ]
