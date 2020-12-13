947781628094b739f97797834531a4173ab15f67
Logan McDonald <loganmcdona11@gmail.com>
2020-12-13 08:36:07 -0500

    2020/13: part 1

--- 2020/13/solution.py
+++ 2020/13/solution.py
@@ -1,9 +1,32 @@
+import copy
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
+def bus_arriving(ed, schedule):
+    for bus in schedule:
+        if ed % bus == 0:
+            return bus
+
+
+def calculate_bus_departure(earliest_departure, schedule):
+    ed = copy.copy(earliest_departure)
+    while True:
+        ed += 1
+        if ed == 100:
+            break
+        bus = bus_arriving(ed, schedule)
+        if bus:
+            return bus * (ed - earliest_departure)
+            break
+
+
 def answer(problem_input, level, test=False):
-    return 0
+    earliest_departure, schedule = problem_input.splitlines()
+    schedule = [int(i) for i in schedule.replace('x', '').split(',') if i]
+    bus_departure = calculate_bus_departure(int(earliest_departure), schedule)
+    return bus_departure
 
 
 aoc_utils.run(answer, cases)
--- 2020/13/tests.py
+++ 2020/13/tests.py
@@ -1,3 +1,3 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': "939\n7,13,x,x,59,x,31,19", 'output': 295},
 ]
