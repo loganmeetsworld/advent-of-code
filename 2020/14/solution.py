77a581a64c5f96e99d10242de4409f8567f698fc
Logan McDonald <loganmcdona11@gmail.com>
2020-12-14 09:27:09 -0500

    2020/14: setup p2

--- 2020/14/solution.py
+++ 2020/14/solution.py
@@ -4,8 +4,7 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
-def run_program(mask, decimal):
-    value = f'{int(decimal):036b}'
+def run_1(mask, value):
     result = ''
     for x, y in zip(mask, value):
         if x == 'X' and y == '0':
@@ -18,13 +17,20 @@ def run_program(mask, decimal):
     return int(result, 2)
 
 
+def run_2(mask, value):
+    return 0
+
+
 def answer(problem_input, level, test=False):
     addresses = {}
     for program in [p.rstrip('\n') for p in problem_input.split('mask = ') if p]:
         bitmask, rules = program.splitlines()[0], [re.findall(r'\d+', i) for i in program.splitlines()[1:]]
         for rule in rules:
             memory_address, decimal = rule
-            addresses[memory_address] = run_program(bitmask, decimal)
+            if level == 1:
+                addresses[memory_address] = run_1(bitmask, f'{int(decimal):036b}')
+            else:
+                addresses[memory_address] = run_2(bitmask, f'{int(decimal):036b}')
 
     return sum(addresses.values())
 
--- 2020/14/tests.py
+++ 2020/14/tests.py
@@ -1,3 +1,4 @@
 cases = [
     {'level': 1, 'input': "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\nmem[8] = 11\nmem[7] = 101\nmem[8] = 0", 'output': 165},
+    {'level': 2, 'input': "mask = 000000000000000000000000000000X1001X\nmem[42] = 100\nmask = 00000000000000000000000000000000X0XX\nmem[26] = 1", 'output': 208}
 ]
