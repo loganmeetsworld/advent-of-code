0450496478d2e01730bc976736f910aa3aded52b
Logan McDonald <loganmcdona11@gmail.com>
2020-12-14 09:23:19 -0500

    2020/14: part 1

--- 2020/14/solution.py
+++ 2020/14/solution.py
@@ -4,23 +4,29 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
-def run_program(bitmask, rules):
-    for rule in rules:
-        memory_address, decimal = rule
-        decimal_as_binary = f'{int(decimal):036b}'
-        for x, y in zip(bitmask, decimal_as_binary):
-            print(x)
-            print(y)
-    return 0
+def run_program(mask, decimal):
+    value = f'{int(decimal):036b}'
+    result = ''
+    for x, y in zip(mask, value):
+        if x == 'X' and y == '0':
+            result += '0'
+        elif x == 'X' and y == '1':
+            result += '1'
+        else:
+            result += x
+
+    return int(result, 2)
 
 
 def answer(problem_input, level, test=False):
-    sums = []
+    addresses = {}
     for program in [p.rstrip('\n') for p in problem_input.split('mask = ') if p]:
         bitmask, rules = program.splitlines()[0], [re.findall(r'\d+', i) for i in program.splitlines()[1:]]
-        sums.append(run_program(bitmask, rules))
+        for rule in rules:
+            memory_address, decimal = rule
+            addresses[memory_address] = run_program(bitmask, decimal)
 
-    return sum(sums)
+    return sum(addresses.values())
 
 
 aoc_utils.run(answer, cases)
