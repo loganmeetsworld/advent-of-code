5df4ffbe06e0fa60243c87c9091b4f4b00d97e70
Logan McDonald <loganmcdona11@gmail.com>
2020-12-14 08:58:11 -0500

    2020/14 progress

--- 2020/14/solution.py
+++ 2020/14/solution.py
@@ -5,14 +5,19 @@ from tests import cases
 
 
 def run_program(bitmask, rules):
+    for rule in rules:
+        memory_address, decimal = rule
+        decimal_as_binary = f'{int(decimal):036b}'
+        for x, y in zip(bitmask, decimal_as_binary):
+            print(x)
+            print(y)
     return 0
 
 
 def answer(problem_input, level, test=False):
     sums = []
     for program in [p.rstrip('\n') for p in problem_input.split('mask = ') if p]:
-        program = program.splitlines()
-        bitmask, rules = program[0], [re.findall(r'\d+', i) for i in program[1:]]
+        bitmask, rules = program.splitlines()[0], [re.findall(r'\d+', i) for i in program.splitlines()[1:]]
         sums.append(run_program(bitmask, rules))
 
     return sum(sums)
