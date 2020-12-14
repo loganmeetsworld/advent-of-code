e590c394cc7b94483cab3d734ca1f1763b4423d2
Logan McDonald <loganmcdona11@gmail.com>
2020-12-14 08:44:14 -0500

    2020/14: add tests and basic parsing

--- 2020/14/solution.py
+++ 2020/14/solution.py
@@ -1,9 +1,21 @@
+import re
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
-def answer(problem_input, level, test=False):
+def run_program(bitmask, rules):
     return 0
+
+
+def answer(problem_input, level, test=False):
+    sums = []
+    for program in [p.rstrip('\n') for p in problem_input.split('mask = ') if p]:
+        program = program.splitlines()
+        bitmask, rules = program[0], [re.findall(r'\d+', i) for i in program[1:]]
+        sums.append(run_program(bitmask, rules))
+
+    return sum(sums)
 
 
 aoc_utils.run(answer, cases)
--- 2020/14/tests.py
+++ 2020/14/tests.py
@@ -1,3 +1,3 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': "mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X\nmem[8] = 11\nmem[7] = 101\nmem[8] = 0", 'output': 165},
 ]
