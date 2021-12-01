6009a52296157498c3626f91d7bf1fc38dd668da
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-01 08:35:09 -0500

    2021 day 1 complete

--- 2021/1/solution.py
+++ 2021/1/solution.py
@@ -1,9 +1,33 @@
 from aoc_utils import aoc_utils
 from tests import cases
 
+def count_increased(l):
+    increased_count = 0
+    for idx in range(1, len(l)):
+        if l[idx] - l[idx - 1] > 0:
+            increased_count += 1
+    return increased_count
+
+
+def get_triplet_sums(depths):
+    triplet_sums = []
+    start_range = 0
+    end_range = 2
+    while end_range < len(depths):
+        triplet_sums.append(sum(depths[start_range:end_range + 1]))
+        start_range += 1
+        end_range += 1
+    return triplet_sums
+
 
 def answer(problem_input, level, test=False):
-    return 0
+    depths = [int(i) for i in problem_input.splitlines()]
+
+    if level == 1:
+        return count_increased(depths)
+    elif level == 2:
+        triplets = get_triplet_sums(depths)
+        return count_increased(triplets)
 
 
 aoc_utils.run(answer, cases)
--- 2021/1/tests.py
+++ 2021/1/tests.py
@@ -1,3 +1,4 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': '199\n200\n208\n210\n200\n207\n240\n269\n260\n263', 'output': 7},
+    {'level': 2, 'input': '199\n200\n208\n210\n200\n207\n240\n269\n260\n263', 'output': 5},
 ]
