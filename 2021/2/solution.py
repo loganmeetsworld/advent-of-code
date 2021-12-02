26d981b4c0281381ba8fe792254905dd8502d965
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-02 08:36:08 -0500

    2021 day 2 sloppy version

--- 2021/2/solution.py
+++ 2021/2/solution.py
@@ -3,7 +3,25 @@ from tests import cases
 
 
 def answer(problem_input, level, test=False):
-    return 0
+    horizontal_pos = 0
+    depth = 0
+    aim = 0
+    for i in problem_input.splitlines():
+        command, amount = [i[0:-2], int(i[-1])]
+        if command == 'forward':
+            horizontal_pos += amount
+            if level == 2:
+                depth += (aim * amount)
+        elif command == 'down':
+            if level == 1:
+                depth += amount
+            aim += amount
+        elif command == 'up':
+            if level == 1:
+                depth -= amount
+            aim -= amount
+
+    return horizontal_pos * depth
 
 
 aoc_utils.run(answer, cases)
--- 2021/2/tests.py
+++ 2021/2/tests.py
@@ -1,3 +1,5 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2", 'output': 150},
+    {'level': 2, 'input': "forward 5\ndown 5\nforward 8\nup 3\ndown 8\nforward 2", 'output': 900},
 ]
+
