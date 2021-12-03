a19c30f060ce16359d066db794d95a530754c0a3
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-03 08:39:59 -0500

    2021 day 3 part 1

--- 2021/3/solution.py
+++ 2021/3/solution.py
@@ -3,7 +3,16 @@ from tests import cases
 
 
 def answer(problem_input, level, test=False):
-    return 0
+    gamma_rate, epsilon_rate = '', ''
+    binaries = [i for i in problem_input.splitlines()]
+
+    for b in zip(*binaries):
+        if sum([int(i) for i in b]) > (len(binaries) / 2):
+            gamma_rate += '1'; epsilon_rate += '0'
+        else:
+            gamma_rate += '0'; epsilon_rate += '1'
+
+    return int(gamma_rate, 2) * int(epsilon_rate, 2)
 
 
 aoc_utils.run(answer, cases)
