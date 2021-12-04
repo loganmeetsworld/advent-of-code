c04ff601edbec09b17a9e78c6102eccc8d2e29d8
Logan McDonald <loganmcdona11@gmail.com>
2021-12-04 09:44:40 -0500

    basic setup for day 4

--- 2021/4/solution.py
+++ 2021/4/solution.py
@@ -1,9 +1,38 @@
+import re
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
+class Board():
+    def __init__(self, layout):
+        self.tiles = self.parse_board(layout)
+        self.winner = False
+        self.winning_score = 0
+
+    def parse_board(board):
+        numbers = re.findall(r'(\d+)', board)
+        return [numbers[i:i + 5] for i in range(0, len(numbers), 5)]
+
+    def play(number):
+        # X out number
+        print(number)
+        # Check for win, set winner to True if winner
+
+    def score_board(self):
+        self.winning_score = 0
+        # set winning score to all unused tiles
+
+
 def answer(problem_input, level, test=False):
-    return 0
+    parsed_input = problem_input.split("\n\n")
+    plays = parsed_input[0].split(',')
+    boards = [Board(b) for b in parsed_input[1:]]
+    for number in plays:
+        for board in boards:
+            board.play(number)
+            if board.winner:
+                return board.winning_score
 
 
 aoc_utils.run(answer, cases)
