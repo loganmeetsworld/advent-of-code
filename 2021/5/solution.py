04d892e2bca2d8c9af29df6a8ef6b249852f24d9
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-05 09:00:44 -0500

    2021 day 5 part 1 im sure wont work for part 2

--- 2021/5/solution.py
+++ 2021/5/solution.py
@@ -1,9 +1,24 @@
+from collections import Counter
+
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
 def answer(problem_input, level, test=False):
-    return 0
+    directions = [l.split(' -> ') for l in problem_input.splitlines()]
+    coordinates = []
+    for direction in directions:
+        point_one = [int(n) for n in direction[0].split(',')]
+        point_two = [int(n) for n in direction[1].split(',')]
+        if point_one[0] == point_two[0]:
+            for y in range(min([point_two[1], point_one[1]]), max([point_two[1], point_one[1]]) + 1):
+                coordinates.append([point_one[0], y])
+        if point_one[1] == point_two[1]:
+            for x in range(min([point_two[0], point_one[0]]), max([point_two[0], point_one[0]]) + 1):
+                coordinates.append([x, point_one[1]])
+    coord_counter = dict(Counter([str(c) for c in coordinates]))
+    return len([k for k,v in Counter(coord_counter).items() if v>1])
 
 
 aoc_utils.run(answer, cases)
