b781b01a9e973cf071c1415c484a9db8a17ed120
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-05 09:49:40 -0500

    ignoring diagnols

--- 2021/5/solution.py
+++ 2021/5/solution.py
@@ -6,24 +6,26 @@ from collections import Counter
 from aoc_utils import aoc_utils
 from tests import cases
 
-def points_between(x1, y1, x2, y2):
-    coordinates = []
+
+def horrizontal_points(x1, y1, x2, y2):
+    points = []
 
     if x1 - x2 == 0:
         for y in range(min([y2, y1]), max([y2, y1]) + 1):
-            coordinates.append(str([x1, y]))
+            points.append(str([x1, y]))
     if y1 - y2 == 0:
         for x in range(min([x2, x1]), max([x2, x1]) + 1):
-            coordinates.append(str([x, y1]))
+            points.append(str([x, y1]))
 
-    return coordinates
+    return points
 
 
 def answer(problem_input, level, test=False):
     coordinates = []
     for line in problem_input.splitlines():
-        coordinates += points_between(*[int(i) for i in re.findall(r'(\d+)', line)])
+        coordinates += horrizontal_points(*[int(i) for i in re.findall(r'(\d+)', line)])
 
+    print(coordinates)
     return len([k for k,v in Counter(coordinates).items() if v > 1])
 
 
