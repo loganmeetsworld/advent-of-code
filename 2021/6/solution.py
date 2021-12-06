de7f0f8a9d66293f4ae0a5716a82de1b3218ebff
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-06 08:25:09 -0500

    brute force

--- 2021/6/solution.py
+++ 2021/6/solution.py
@@ -2,8 +2,34 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
+class Lanternfish():
+    def __init__(self, birth_cycle):
+        self.birth_cycle = birth_cycle
+    def grow(self):
+        self.birth_cycle -= 1
+    def birth(self):
+        self.birth_cycle = 7
+    def pregnant(self):
+        return self.birth_cycle == 0
+
+
 def answer(problem_input, level, test=False):
-    return 0
+    if level == 1:
+        days = 80
+    elif level == 2:
+        days = 256
+    
+    fishies = [Lanternfish(int(i)) for i in problem_input.split(',')]
+
+    while days > 0:
+        for fish in fishies:
+            if fish.pregnant():
+                fishies.append(Lanternfish(9))
+                fish.birth()
+            fish.grow()
+        days -= 1
+    
+    return len(fishies)
 
 
 aoc_utils.run(answer, cases)
--- 2021/6/tests.py
+++ 2021/6/tests.py
@@ -1,3 +1,4 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': '3,4,3,1,2', 'output': 5934},
+    {'level': 2, 'input': '3,4,3,1,2', 'output': 26984457539},
 ]
