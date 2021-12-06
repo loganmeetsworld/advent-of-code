dd2312622f27ac2f0c81f00089f968a88828ce57
Logan McDonald <loganmcdona11@gmail.com>
2021-12-05 19:41:55 -0500

    came back to add diagnols

--- 2021/5/solution.py
+++ 2021/5/solution.py
@@ -7,6 +7,48 @@ from aoc_utils import aoc_utils
 from tests import cases
 
 
+def find_all_coordinates(x1, y1, x2, y2, part_one=False):
+    points = []
+
+    dx = x1 - x2
+    dy = y1 - y2 
+
+    # Like 2,2 -> 2,1
+    if dx == 0:
+        for y in range(min([y2, y1]), max([y2, y1]) + 1):
+            points.append(str([x1, y]))
+
+    # Like 0,9 -> 5,9
+    if dy == 0:
+        for x in range(min([x2, x1]), max([x2, x1]) + 1):
+            points.append(str([x, y1]))
+
+    if part_one:
+        return points
+    
+    # Like 1,1 -> 3,3
+    if dx < 0 and dy < 0:
+        for i in range(abs(dx) + 1):
+            points.append(str([x1 + i, y1 + i]))
+
+    # Like 3,3 -> 1,1
+    if dx > 0 and dy > 0:
+        for i in range(abs(dx) + 1):
+            points.append(str([x1 - i, y1 - i]))
+
+    # Like 9,7 -> 7,9
+    if dx > 0 and dy < 0:
+        for i in range(abs(dx) + 1):
+            points.append(str([x1 - i, y1 + i]))
+
+    # Like 7,9 > 9,7
+    if dx < 0 and dy > 0:
+        for i in range(abs(dx) + 1):
+            points.append(str([x1 + i, y1 - i]))
+
+    return points
+
+
 def horrizontal_points(x1, y1, x2, y2):
     points = []
 
@@ -23,9 +65,12 @@ def horrizontal_points(x1, y1, x2, y2):
 def answer(problem_input, level, test=False):
     coordinates = []
     for line in problem_input.splitlines():
-        coordinates += horrizontal_points(*[int(i) for i in re.findall(r'(\d+)', line)])
+        coords = [int(i) for i in re.findall(r'(\d+)', line)]
+        if level == 1:
+            coordinates += find_all_coordinates(*coords, part_one=True)
+        else:
+            coordinates += find_all_coordinates(*coords)
 
-    print(coordinates)
     return len([k for k,v in Counter(coordinates).items() if v > 1])
 
 
