059c3669081dfbf855b5074e1a1c42cc05167aa4
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-07 08:38:28 -0500

    math makes no sense

--- 2021/7/solution.py
+++ 2021/7/solution.py
@@ -1,3 +1,4 @@
+import math
 import statistics
 
 from aoc_utils import aoc_utils
@@ -7,7 +8,13 @@ from tests import cases
 def answer(problem_input, level, test=False):
     horizontal_distances = [int(i) for i in problem_input.split(',')]
     median = statistics.median(horizontal_distances)
-    return int(sum([abs(median - i) for i in horizontal_distances]))
-
+    # Have absolutely no idea what is happening here that the test fails but got the star.
+    mean = math.floor(statistics.mean(horizontal_distances))
+    if level == 1:
+        distances_from_median = [abs(median - i) for i in horizontal_distances]
+        return int(sum(distances_from_median))
+    elif level == 2:
+        distances_from_mean = [abs(mean - i) for i in horizontal_distances]
+        return int(sum([int(d * (d + 1) / 2) for d in distances_from_mean]))
 
 aoc_utils.run(answer, cases)
