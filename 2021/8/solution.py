4d6a7b30d4bf8a333a4628655d393091693f6e8d
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-08 09:01:43 -0500

    2021 day 8 part 1

--- 2021/8/solution.py
+++ 2021/8/solution.py
@@ -3,7 +3,10 @@ from tests import cases
 
 
 def answer(problem_input, level, test=False):
-    return 0
+    outputs, acceptible_lengths = [], [2, 3, 4, 7]
+    for line in problem_input.splitlines():
+        outputs += line.split(' | ')[1].split()
+    return sum(1 for i in outputs if len(i) in acceptible_lengths)
 
 
 aoc_utils.run(answer, cases)
