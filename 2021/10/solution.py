410433c9ecc226c379c0a4ea4cd34e1ffa7c24c2
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-10 10:52:25 -0500

    add some helpful comments

--- 2021/10/solution.py
+++ 2021/10/solution.py
@@ -16,31 +16,31 @@ POINTS = {
 def score_incomplete(chunk):
     autocomplete_score = 0
     tr = str.maketrans('(){}<>[]', ')(}{><][')
-    reversed_chunk = chunk[::-1].translate(tr)
-    for char in list(reversed_chunk):
+    for char in list(chunk[::-1].translate(tr)):
         autocomplete_score = (autocomplete_score * 5) + POINTS[char][1]
-    
     return autocomplete_score
 
 
 def answer(problem_input, level, test=False):
     syntax_scores, autocomplete_scores = [], []
-
     for chunk in problem_input.splitlines():
+        # Remove all adjacent pairs () [] <> {} until none are left
         while True:
             prev_len = len(chunk)
             chunk = re.sub(BRACKET_REGEX, '', chunk)
             if prev_len == len(chunk): break
 
+        # The corrupted ones will include hanging ending brackets ) ] > }
         corrupted = re.search(END_BRACKET_REGEX, chunk)
         if corrupted: 
             syntax_scores.append(POINTS[corrupted.group(0)][0])
+        # Incomplete ones have no loose ending brackets and just need to be autocompleted
         else:
             autocomplete_scores.append(score_incomplete(chunk))
 
     if level == 1:
         return sum(syntax_scores)
-    else:
+    elif level == 2:
         return median(autocomplete_scores)
 
 
