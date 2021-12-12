6d881d2157e8b251ba5b62fb0367635f6c679b5b
Logan McDonald <loganmcdona11@gmail.com>
2021-12-12 10:11:59 -0500

    setup day 12

--- 2021/12/solution.py
+++ 2021/12/solution.py
@@ -1,9 +1,11 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
 def answer(problem_input, level, test=False):
     return 0
-
-
+    
 aoc_utils.run(answer, cases)
--- 2021/12/tests.py
+++ 2021/12/tests.py
@@ -1,3 +1,44 @@
+TEST_INPUT_1 = """start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"""
+
+TEST_INPUT_2 = """dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"""
+
+TEST_INPUT_3 = """fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW"""
+
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': TEST_INPUT_1, 'output': 10},
+    # {'level': 1, 'input': TEST_INPUT_2, 'output': 19},
+    # {'level': 1, 'input': TEST_INPUT_3, 'output': 226},
+    # {'level': 2, 'input': TEST_INPUT_1, 'output': 0},
 ]
