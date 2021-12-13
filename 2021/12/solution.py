e32319fe53d4627cb0e756a41c389ce9deead79c
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-13 07:53:54 -0800

    setup 12

--- 2021/12/solution.py
+++ 2021/12/solution.py
@@ -1,9 +1,23 @@
+from collections import defaultdict
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
-def answer(problem_input, level, test=False):
+def search(graph, node):
+    # do something
     return 0
+        
 
+def answer(problem_input, level, test=False):
+    graph = defaultdict(list)
+    for line in problem_input.splitlines():
+        origin, dest = line.split('-')
+        if dest != 'start' and origin != 'end':
+            graph[origin].append(dest)
+        if origin != 'start' and dest != 'end':
+            graph[dest].append(origin)
+    print(graph)
+    return search(graph, 'start')
 
 aoc_utils.run(answer, cases)
--- 2021/12/tests.py
+++ 2021/12/tests.py
@@ -1,3 +1,11 @@
+TEST_INPUT_1 = """start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"""
+
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': TEST_INPUT_1, 'output': 10},
 ]
