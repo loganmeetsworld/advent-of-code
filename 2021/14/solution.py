4ed28e14db3fd4827f11d19bc431a02ccfbfb055
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-14 08:03:38 -0800

    day 14 part 1

--- 2021/14/solution.py
+++ 2021/14/solution.py
@@ -1,22 +1,37 @@
+import re
+
 from collections import Counter
 
 from aoc_utils import aoc_utils
 from tests import cases
 
 
+def transform(polymer, rules_hash):
+    idx = 0
+    while idx < len(polymer):
+        next_phrase = polymer[idx:idx+2]
+        if ''.join(next_phrase) in rules_hash.keys():
+            polymer.insert(idx+1, rules_hash[''.join(next_phrase)])
+            idx += 2
+        else:
+            idx += 1
 
-def transform(polymer, rules):
     return polymer
 
 
 def answer(problem_input, level, test=False):
     steps = 10
     polymer, rules = problem_input.split("\n\n")
-    for _ in range(steps):
-        polymer = transform(polymer, [r.split(' -> ') for r in rules.splitlines()])
+    rules_hash = {}
+    for r in rules.splitlines():
+        k, v = r.split(' -> ')
+        rules_hash[k] = v
 
-    counter = Counter(polymer).most_common()
-    return counter[0][1] - counter[-1][1]
+    for _ in range(steps):
+        polymer = transform(list(polymer), rules_hash)
+
+    counter = Counter(polymer).values()
+    return max(counter) - min(counter)
 
 
 aoc_utils.run(answer, cases)
