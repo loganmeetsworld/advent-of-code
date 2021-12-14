2f12484eab051945eded7173fe60e906371c5069
LoganMMcDonald <logan.mcdonald@gsa.gov>
2021-12-13 21:26:51 -0800

    setup

--- 2021/14/solution.py
+++ 2021/14/solution.py
@@ -1,9 +1,22 @@
+from collections import Counter
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
+
+def transform(polymer, rules):
+    return polymer
+
+
 def answer(problem_input, level, test=False):
-    return 0
+    steps = 10
+    polymer, rules = problem_input.split("\n\n")
+    for _ in range(steps):
+        polymer = transform(polymer, [r.split(' -> ') for r in rules.splitlines()])
+
+    counter = Counter(polymer).most_common()
+    return counter[0][1] - counter[-1][1]
 
 
 aoc_utils.run(answer, cases)
--- 2021/14/tests.py
+++ 2021/14/tests.py
@@ -1,3 +1,22 @@
+TEST_INPUT_1 = """NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C"""
+
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': TEST_INPUT_1, 'output': 1588},
 ]
