e3fc48fd993cdcf2f676615027d936d8f78fc6a5
Logan McDonald <loganmcdona11@gmail.com>
2022-12-02 08:16:36 -0500

    i love mess

--- 2022/2/solution.py
+++ 2022/2/solution.py
@@ -1,9 +1,45 @@
 from aoc_utils import aoc_utils
 from tests import cases
 
+def translate_input(lines):
+    lookup = {
+        'X': 'A',
+        'Y': 'B',
+        'Z': 'C'
+    }
+    players = []
+    for line in lines:
+        players.append([line.split(' ')[0], lookup[line.split(' ')[1]]])
+
+    return players
+
+def score_game(game):
+    win = 6
+    lose = 0
+    draw = 3
+    score_guide = {
+        'A': 1,
+        'B': 2,
+        'C': 3,
+        'AB': lose,
+        'BA': win,
+        'AC': win,
+        'CA': lose,
+        'BC': lose,
+        'CB': win,
+        'AA': draw,
+        'BB': draw,
+        'CC': draw
+    }
+
+    return score_guide[game[1]] + score_guide[''.join(game)]
 
 def answer(problem_input, level, test=False):
-    return 0
+    total_score = 0
+    games = translate_input(problem_input.splitlines())
+    for game in games:
+        total_score += score_game(game)
+    return total_score
 
 
 aoc_utils.run(answer, cases)
--- 2022/2/tests.py
+++ 2022/2/tests.py
@@ -1,3 +1,4 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': "A Y\nB X\nC Z", 'output': 15},
+    {'level': 2, 'input': "A Y\nB X\nC Z", 'output': 12},
 ]
