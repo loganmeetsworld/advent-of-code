8d23169dd04ef3630ff320808df5a7e8b374055e
Logan McDonald <loganmcdona11@gmail.com>
2022-12-04 09:21:54 -0500

    day 4 part 1

--- 2022/4/solution.py
+++ 2022/4/solution.py
@@ -1,9 +1,21 @@
 from aoc_utils import aoc_utils
 from tests import cases
 
+def process_pair(pair):
+    pair = [elem.split('-') for elem in pair.split(',')]
+    A = set(range(int(pair[0][0]), int(pair[0][1]) + 1))
+    B = set(range(int(pair[1][0]), int(pair[1][1]) + 1))
+    return A, B
+
 
 def answer(problem_input, level, test=False):
-    return 0
+    superset_count = 0
+    for pair in problem_input.splitlines():
+        A, B = process_pair(pair)
+        if A.issuperset(B) or B.issuperset(A):
+            superset_count += 1
+
+    return superset_count
 
 
 aoc_utils.run(answer, cases)
