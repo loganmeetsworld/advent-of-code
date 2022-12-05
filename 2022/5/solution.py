5699242e1bafd0b50ed1d90cb9cd1a4642ebbdb2
Logan McDonald <loganmcdona11@gmail.com>
2022-12-05 10:03:44 -0500

    attempt

--- 2022/5/solution.py
+++ 2022/5/solution.py
@@ -1,9 +1,45 @@
+from collections import deque
+import numpy as np
+import re
+
 from aoc_utils import aoc_utils
 from tests import cases
 
 
+def create_stacks(crane_map):
+    places = [1]
+    while len(crane_map[0]) > places[-1] + 4:
+        places.append(places[-1] + 4)
+
+    cranes = []
+    for crane in crane_map:
+        crane_row = []
+        for place in places:
+            crane_row.append(crane[place])
+        cranes.append(crane_row)
+
+    rows, columns = len(cranes), len(cranes[0])
+    stacks = []
+    for j in range(columns):
+        row = []
+        for i in range(rows):
+            if cranes[i][j] != " ":
+                row.append(cranes[i][j])
+        stacks.append(row)
+    return stacks
+
+
 def answer(problem_input, level, test=False):
-    return 0
+    crane_map, directions = problem_input.split('\n\n')
+    stacks =  create_stacks(crane_map.splitlines()[:-1])
+
+    for direction in directions.splitlines():
+        num_to_move, from_idx, to_idx = re.findall("(\d+)", direction)
+        for _ in range(int(num_to_move)):
+            pop = stacks[int(from_idx) - 1].pop(0)
+            stacks[int(to_idx) -1].insert(0, pop)
+                    
+    return "".join([crane[0] for crane in stacks])
 
 
 aoc_utils.run(answer, cases)
--- 2022/5/tests.py
+++ 2022/5/tests.py
@@ -1,3 +1,3 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': "    [D]    \n[N] [C]    \n[Z] [M] [P]\n 1   2   3 \n\nmove 1 from 2 to 1\nmove 3 from 1 to 3\nmove 2 from 2 to 1\nmove 1 from 1 to 2", 'output': "CMZ"},
 ]
