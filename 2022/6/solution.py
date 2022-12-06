0ea915d6525578d006776231d9734631d223d2d9
LoganMMcDonald <logan.mcdonald@gsa.gov>
2022-12-06 07:06:38 -0500

    part 2

--- 2022/6/solution.py
+++ 2022/6/solution.py
@@ -3,9 +3,13 @@ from tests import cases
 
 
 def answer(problem_input, level, test=False):
+    if level == 1:
+        char_count = 4
+    elif level == 2:
+        char_count = 14
+
     datastream_buffer = list(problem_input)
-    four_chunk, remaining_buffer = datastream_buffer[0:4], datastream_buffer[4:]
-    char_count = 4
+    four_chunk, remaining_buffer = datastream_buffer[0:char_count], datastream_buffer[char_count:]
     while len(set(four_chunk)) != len(four_chunk):
         popped = remaining_buffer.pop(0)
         four_chunk.pop(0)
--- 2022/6/tests.py
+++ 2022/6/tests.py
@@ -4,4 +4,11 @@ cases = [
     {'level': 1, 'input': 'nppdvjthqldpwncqszvftbrmjlhg', 'output': 6},
     {'level': 1, 'input': 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 'output': 10},
     {'level': 1, 'input': 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 'output': 11},
+
+    {'level': 2, 'input': 'mjqjpqmgbljsphdztnvjfqwrcgsmlb', 'output': 19},
+    {'level': 2, 'input': 'bvwbjplbgvbhsrlpgdmjqwftvncz', 'output': 23},
+    {'level': 2, 'input': 'nppdvjthqldpwncqszvftbrmjlhg', 'output': 23},
+    {'level': 2, 'input': 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 'output': 29},
+    {'level': 2, 'input': 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 'output': 26},
+
 ]
