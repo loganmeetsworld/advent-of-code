e3be38f6618ef39b133f375a4468a6facf3f805f
LoganMMcDonald <logan.mcdonald@gsa.gov>
2022-12-06 06:52:10 -0500

    setup and write tests

--- 2022/6/tests.py
+++ 2022/6/tests.py
@@ -1,3 +1,7 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': 'mjqjpqmgbljsphdztnvjfqwrcgsmlb', 'output': 7},
+    {'level': 1, 'input': 'bvwbjplbgvbhsrlpgdmjqwftvncz', 'output': 5},
+    {'level': 1, 'input': 'nppdvjthqldpwncqszvftbrmjlhg', 'output': 6},
+    {'level': 1, 'input': 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 'output': 10},
+    {'level': 1, 'input': 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 'output': 11},
 ]
