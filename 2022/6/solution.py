f855a56fbf538600fe899d0858d0a16765f86901
LoganMMcDonald <logan.mcdonald@gsa.gov>
2022-12-06 07:03:06 -0500

    part 1

--- 2022/6/solution.py
+++ 2022/6/solution.py
@@ -3,7 +3,15 @@ from tests import cases
 
 
 def answer(problem_input, level, test=False):
-    return 0
+    datastream_buffer = list(problem_input)
+    four_chunk, remaining_buffer = datastream_buffer[0:4], datastream_buffer[4:]
+    char_count = 4
+    while len(set(four_chunk)) != len(four_chunk):
+        popped = remaining_buffer.pop(0)
+        four_chunk.pop(0)
+        four_chunk.append(popped)
+        char_count += 1
+    return char_count
 
 
 aoc_utils.run(answer, cases)
