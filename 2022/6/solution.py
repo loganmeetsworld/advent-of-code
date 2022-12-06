9b7b9094d28b87f4a163682208aad5205a0e2173
LoganMMcDonald <logan.mcdonald@gsa.gov>
2022-12-06 07:07:28 -0500

    tidy

--- 2022/6/solution.py
+++ 2022/6/solution.py
@@ -3,11 +3,7 @@ from tests import cases
 
 
 def answer(problem_input, level, test=False):
-    if level == 1:
-        char_count = 4
-    elif level == 2:
-        char_count = 14
-
+    char_count = 4 if level == 1 else char_count = 14
     datastream_buffer = list(problem_input)
     four_chunk, remaining_buffer = datastream_buffer[0:char_count], datastream_buffer[char_count:]
     while len(set(four_chunk)) != len(four_chunk):
