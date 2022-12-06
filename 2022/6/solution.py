99afea945155e27799383206d07839e5d4c1b6ac
LoganMMcDonald <logan.mcdonald@gsa.gov>
2022-12-06 08:38:42 -0500

    succinct

--- 2022/6/solution.py
+++ 2022/6/solution.py
@@ -3,13 +3,12 @@ from tests import cases
 
 
 def answer(problem_input, level, test=False):
-    char_count = 4 if level == 1 else char_count = 14
-    datastream_buffer = list(problem_input)
-    four_chunk, remaining_buffer = datastream_buffer[0:char_count], datastream_buffer[char_count:]
-    while len(set(four_chunk)) != len(four_chunk):
-        popped = remaining_buffer.pop(0)
-        four_chunk.pop(0)
-        four_chunk.append(popped)
+    char_count = 4 if level == 1 else 14
+    chunk, remaining_buffer = list(problem_input[0:char_count]), list(problem_input[char_count:])
+    while len(set(chunk)) != len(chunk):
+        char = remaining_buffer.pop(0)
+        chunk.pop(0)
+        chunk.append(char)
         char_count += 1
     return char_count
 
