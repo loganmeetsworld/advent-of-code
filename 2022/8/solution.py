b631866ae63d8d2e92f33b050d2620109c0268ee
LoganMMcDonald <logan.mcdonald@gsa.gov>
2022-12-08 16:35:02 -0500

    passes tests

--- 2022/8/solution.py
+++ 2022/8/solution.py
@@ -3,7 +3,72 @@ from tests import cases
 
 
 def answer(problem_input, level, test=False):
-    return 0
+    tree_map = [list(col) for col in problem_input.splitlines()]
+    visible_count = 0
+    scenic_scores = []
+    for rowi, row in enumerate(tree_map):
+        for coli, col in enumerate(row):
+            ups_counter = 1
+            ups_visible = 0
+            heighest_up_seen = 9
+            ups = []
+            while rowi - ups_counter >= 0:
+                # print(int(tree_map[rowi - ups_counter][coli]))
+                if int(tree_map[rowi - ups_counter][coli]) <= heighest_up_seen:
+                    ups_visible += 1
+                    heighest_up_seen = int(tree_map[rowi - ups_counter][coli])
+                ups.append(tree_map[rowi - ups_counter][coli])
+                ups_counter += 1
+
+            downs_counter = 1
+            downs_visible = 0
+            heighest_down_seen = 9
+            downs = []
+            while rowi + downs_counter < len(tree_map):
+                if int(tree_map[rowi + downs_counter][coli]) <= heighest_down_seen:
+                    downs_visible += 1
+                    heighest_down_seen = int(tree_map[rowi + downs_counter][coli])
+
+                downs.append(tree_map[rowi + downs_counter][coli])
+                downs_counter += 1
+
+            rights_counter = 1
+            rights_visible = 0
+            heighest_right_seen = 9
+            rights = []
+            while coli + rights_counter < len(row):
+                if int(tree_map[rowi][coli + rights_counter]) <= heighest_right_seen:
+                    rights_visible += 1
+                    heighest_right_seen = int(tree_map[rowi][coli + rights_counter])
+
+                rights.append(tree_map[rowi][coli + rights_counter])
+                rights_counter += 1
+
+            lefts_counter = 1
+            lefts_visible = 0
+            heighest_left_seen = 9
+            lefts = []
+            while coli - lefts_counter >= 0:
+                if int(tree_map[rowi][coli - lefts_counter]) <= heighest_left_seen:
+                    lefts_visible += 1
+                    heighest_left_seen = int(tree_map[rowi][coli - lefts_counter])
+
+                lefts.append(tree_map[rowi][coli - lefts_counter])
+                lefts_counter += 1
+
+            if coli == 0 or rowi == 0 or coli + 1 == len(row) or len(tree_map) == rowi + 1:
+                visible_count += 1
+                continue
+
+            if col > max(ups) or col > max(downs) or col > max(lefts) or col > max(rights):
+                visible_count += 1
+
+            scenic_scores.append(downs_visible * ups_visible * rights_visible * lefts_visible)
+
+    if level == 1:
+        return visible_count
+    else:
+        return max(scenic_scores)
 
 
 aoc_utils.run(answer, cases)
--- 2022/8/tests.py
+++ 2022/8/tests.py
@@ -1,3 +1,4 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': '30373\n25512\n65332\n33549\n35390', 'output': 21},
+    {'level': 2, 'input': '30373\n25512\n65332\n33549\n35390', 'output': 8},
 ]
