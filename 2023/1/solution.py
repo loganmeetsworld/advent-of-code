c372b077fed608d9180f29926bac10f31aad0d2f
Logan McDonald <loganmcdona11@gmail.com>
2023-11-30 22:04:32 -0800

    part 1

--- 2023/1/solution.py
+++ 2023/1/solution.py
@@ -1,9 +1,32 @@
+import re
+
 from aoc_utils import aoc_utils
 from tests import cases
 
+def word_to_number(number):
+    words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"]
+    for i in words:
+        number = number.replace(i, str(words.index(i)))
+    return number
 
 def answer(problem_input, level, test=False):
-    return 0
+    numbers_list = []
+
+    if level == 1:
+        for line in problem_input.splitlines():
+            numbers = re.findall("\d", line)
+            first, last = numbers[0], numbers[-1]
+            numbers_list.append(int(first + last))
+    elif level == 2:
+        for line in problem_input.splitlines():
+            numbers = re.findall("(one|two|three|four|five|six|seven|eight|nine|\d)", line)
+            numbers_string = ''
+            for number in numbers:
+                numbers_string += word_to_number(number)
+            first, last = numbers_string[0], numbers_string[-1]
+            numbers_list.append(int(first + last))
+
+    return sum(numbers_list)            
 
 
 aoc_utils.run(answer, cases)
--- 2023/1/tests.py
+++ 2023/1/tests.py
@@ -1,3 +1,13 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': """1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet""", 'output': 142},
+    {'level': 2, 'input': """two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen""", 'output': 281}
 ]
