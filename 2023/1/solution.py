95c74b233e4c24f3729b1242bf18defe853429d3
Logan McDonald <loganmcdona11@gmail.com>
2023-12-01 09:01:30 -0800

    refactor a bit

--- 2023/1/solution.py
+++ 2023/1/solution.py
@@ -3,31 +3,28 @@ import re
 from aoc_utils import aoc_utils
 from tests import cases
 
-def word_to_number(number):
+def normalize_numbers(line):
+    numbers = re.findall("(?=(one|two|three|four|five|six|seven|eight|nine|\d))", line)
     words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    for i in words:
-        number = number.replace(i, str(words.index(i)))
-    return number
+    normalized_numbers = ''
+    for number in numbers:
+        if number.isnumeric(): 
+            normalized_numbers += number
+        else:
+            normalized_numbers += number.replace(number, str(words.index(number)))
+    return normalized_numbers
 
 def answer(problem_input, level, test=False):
-
-    if level == 1:
-        numbers_list = []
-        for line in problem_input.splitlines():
+    numbers_list = []
+    for line in problem_input.splitlines():
+        if level == 1:
             numbers = re.findall("\d", line)
-            first, last = numbers[0], numbers[-1]
-            numbers_list.append(int(first + last))
-        return sum(numbers_list)            
-    elif level == 2:
-        numbers_list = []
-        for line in problem_input.splitlines():
-            numbers = re.findall("(?=(one|two|three|four|five|six|seven|eight|nine|\d))", line)
-            numbers_string = ''
-            for number in numbers:
-                numbers_string += word_to_number(number)
-            first, last = numbers_string[0], numbers_string[-1]
-            numbers_list.append(int(first + last))
-        return sum(numbers_list)
+        elif level == 2:
+            numbers = normalize_numbers(line)
+        first, last = numbers[0], numbers[-1]
+        numbers_list.append(int(first + last))
+ 
+    return sum(numbers_list)
 
 
 aoc_utils.run(answer, cases)
