193fe145d5b93f053bda880a7c6084d090d6277f
Logan McDonald <loganmcdona11@gmail.com>
2023-12-04 21:16:53 -0800

    part 2 wip

--- 2023/4/solution.py
+++ 2023/4/solution.py
@@ -3,20 +3,31 @@ from tests import cases
 
 import re
 
-
-def answer(problem_input, level, test=False):
+def calculate_points(problem_input):
     all_points = 0
     for card in problem_input.splitlines():
         my_numbers, winning_numbers = [re.findall("\d+", n) for n in card.split(':')[1].split('|')]
         win_count = len(set(my_numbers) & set(winning_numbers))
         if win_count > 0:
-            points = 1
+            card_points = 1
             for i in range(1, win_count):
                 points *= 2
                 
-            all_points += points
+            all_points += card_points
 
     return all_points
+
+
+def answer(problem_input, level, test=False):
+    if level == 1:
+        return calculate_points(problem_input)
+    elif level == 2:
+        cards = []
+        for card in problem_input.splitlines():
+            card_number = int(card.split('Card ')[0])
+            my_numbers, winning_numbers = [re.findall("\d+", n) for n in card.split(':')[1].split('|')]
+            matches = len(set(my_numbers) & set(winning_numbers))
+            range(card_number + 1, card_number + matches)
 
 
 aoc_utils.run(answer, cases)
