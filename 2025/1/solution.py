9a9801425f9b38b8c7dec53448fee64875c114c4
Logan McDonald <loganmcdona11@gmail.com>
2025-12-01 18:49:27 -0800

    day 1

--- 2025/1/solution.py
+++ 2025/1/solution.py
@@ -1,9 +1,42 @@
 from aoc_utils import aoc_utils
 from tests import cases
 
+def rotate_forward(arr, k):
+    n = len(arr)
+    k = k % n
+    return arr[k:] + arr[:k]
+
+def rotate_backward(arr, k):
+    n = len(arr)
+    k = k % n
+    return arr[n - k:] + arr[:n - k]
 
 def answer(problem_input, level, test=False):
-    return 0
+    arr = rotate_forward(list(range(0,100)), 50)
+    part_2_count = 0
+    part_1_count = 0
+    for line in problem_input.splitlines():
+        direction = line[0]
+        ticks = int(line[1:])
+        if direction == "R":
+            for i in range(ticks):
+                arr = rotate_forward(arr, 1)
+                if arr[0] == 0:
+                    part_2_count += 1 
+            if arr[0] == 0:
+                part_1_count += 1
+        elif direction == "L":
+            for i in range(ticks):
+                arr = rotate_backward(arr, 1)
+                if arr[0] == 0:
+                    part_2_count += 1 
+            if arr[0] == 0:
+                part_1_count += 1
 
-
+    if level == 1: 
+        return part_1_count
+    
+    if level == 2:
+        return part_2_count
+        
 aoc_utils.run(answer, cases)
--- 2025/1/tests.py
+++ 2025/1/tests.py
@@ -1,3 +1,24 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82""", 'output': 3},
+    {'level': 2, 'input': """L68
+L30
+R48
+L5
+R60
+L55
+L1
+L99
+R14
+L82""", 'output': 6},
+    {'level': 2, 'input': """R1000""", 'output': 10},
+
 ]
