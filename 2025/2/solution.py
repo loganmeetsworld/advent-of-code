178cdc28436d81f8c10dac01061949a2ca65f6ba
Logan McDonald <loganmcdona11@gmail.com>
2025-12-01 21:23:05 -0800

    tidy

--- 2025/2/solution.py
+++ 2025/2/solution.py
@@ -1,16 +1,15 @@
 from aoc_utils import aoc_utils
 from tests import cases
 
-def check_id(id):
+def id_has_double_pattern(id):
     return id[:len(id)//2] == id[len(id)//2:]
 
-
-def check_for_invalid_ids(id_range):
+def check_for_invalid_ids_part_1(id_range):
     invalid_ids = []
     start_range, end_range = id_range.split('-')
     for id in range(int(start_range), int(end_range) + 1):
-        if check_id(str(id)):
-            invalid_ids.append(int(id))
+        if id_has_double_pattern(str(id)):
+            invalid_ids.append(id)
 
     return invalid_ids
 
@@ -18,7 +17,8 @@ def answer(problem_input, level, test=False):
     id_ranges = problem_input.split(",")
     invalid_ids = []
     for id_range in id_ranges:
-        invalid_ids += check_for_invalid_ids(id_range)
+        if level == 1:
+            invalid_ids += check_for_invalid_ids_part_1(id_range)
     return sum(invalid_ids)
 
 aoc_utils.run(answer, cases)
