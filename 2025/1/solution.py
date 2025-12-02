32e954b5de5dae448e1405329ef409305597bf11
Logan McDonald <loganmcdona11@gmail.com>
2025-12-01 19:47:19 -0800

    tidy

--- 2025/1/solution.py
+++ 2025/1/solution.py
@@ -12,31 +12,35 @@ def rotate_backward(arr, k):
     return arr[n - k:] + arr[:n - k]
 
 def answer(problem_input, level, test=False):
-    arr = rotate_forward(list(range(0,100)), 50)
-    part_2_count = 0
-    part_1_count = 0
-    for line in problem_input.splitlines():
-        direction = line[0]
-        ticks = int(line[1:])
+    # Set to correct starting position
+    dial_tracker = rotate_forward(list(range(0,100)), 50)
+  
+    # Part one counts if the dial lands at 0 after rotation, part two counts if we ever pass zero while rotating
+    count_landing_at_zero = 0
+    count_passing_through_zero = 0
+
+    for instruction in problem_input.splitlines():
+        direction = instruction[0]
+        ticks = int(instruction[1:])
         if direction == "R":
             for i in range(ticks):
-                arr = rotate_forward(arr, 1)
-                if arr[0] == 0:
-                    part_2_count += 1 
-            if arr[0] == 0:
-                part_1_count += 1
+                dial_tracker = rotate_forward(dial_tracker, 1)
+                if dial_tracker[0] == 0:
+                    count_passing_through_zero += 1 
+            if dial_tracker[0] == 0:
+                count_landing_at_zero += 1
         elif direction == "L":
             for i in range(ticks):
-                arr = rotate_backward(arr, 1)
-                if arr[0] == 0:
-                    part_2_count += 1 
-            if arr[0] == 0:
-                part_1_count += 1
+                dial_tracker = rotate_backward(dial_tracker, 1)
+                if dial_tracker[0] == 0:
+                    count_passing_through_zero += 1 
+            if dial_tracker[0] == 0:
+                count_landing_at_zero += 1
 
     if level == 1: 
-        return part_1_count
+        return count_landing_at_zero
     
     if level == 2:
-        return part_2_count
+        return count_passing_through_zero
         
 aoc_utils.run(answer, cases)
