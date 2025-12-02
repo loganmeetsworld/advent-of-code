48a345b49519ffc288a1eefc4b84e08188508464
Logan McDonald <loganmcdona11@gmail.com>
2025-12-01 19:41:44 -0800

    had to do at least one 2024!

--- 2024/1/solution.py
+++ 2024/1/solution.py
@@ -3,7 +3,33 @@ from tests import cases
 
 
 def answer(problem_input, level, test=False):
-    return 0
+    if level == 1:
+        list_1 = []
+        list_2 = []
+        total = 0
+        for line in problem_input.splitlines():
+            num_1, num_2 = line.split('   ')
+            list_1.append(num_1)
+            list_2.append(num_2)
+        
+        list_1 = sorted(list_1)
+        list_2 = sorted(list_2)
+        for i in range(len(list_1)):
+            total += abs(int(list_1[i]) - int(list_2[i]))
+        return total
+    if level == 2:
+        similarity_score = 0
+        list_1 = []
+        list_2 = []
+        total = 0
+        for line in problem_input.splitlines():
+            num_1, num_2 = line.split('   ')
+            list_1.append(num_1)
+            list_2.append(num_2)
 
+        for i in list_1:
+            similarity_score += (list_2.count(i) * int(i))
+
+        return similarity_score
 
 aoc_utils.run(answer, cases)
--- 2024/1/tests.py
+++ 2024/1/tests.py
@@ -1,3 +1,14 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': """3   4
+4   3
+2   5
+1   3
+3   9
+3   3""", 'output': 11},
+    {'level': 2, 'input': """3   4
+4   3
+2   5
+1   3
+3   9
+3   3""", 'output': 31},
 ]
