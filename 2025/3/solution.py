677a7fce361158f3249bf7e8666c051de7939835
Logan McDonald <loganmcdona11@gmail.com>
2025-12-02 21:13:37 -0800

    part 1

--- 2025/3/solution.py
+++ 2025/3/solution.py
@@ -1,9 +1,17 @@
 from aoc_utils import aoc_utils
 from tests import cases
 
+def find_max_joltage(bank):
+    max_joltage = max(bank[0:-1])
+    index_max_joltage = bank.index(max_joltage)
+    remaining_bank = bank[index_max_joltage+1:len(bank)]
+    return int(max_joltage + max(remaining_bank))
 
 def answer(problem_input, level, test=False):
-    return 0
-
+    banks = problem_input.splitlines()
+    output_joltage = 0
+    for bank in banks:
+        output_joltage += find_max_joltage(bank)
+    return output_joltage
 
 aoc_utils.run(answer, cases)
