0a2324521b4eb023110ac379994236ca58781b72
Logan McDonald <loganmcdona11@gmail.com>
2025-12-06 22:58:41 -0800

    part 2

--- 2025/7/solution.py
+++ 2025/7/solution.py
@@ -1,10 +1,12 @@
 from aoc_utils import aoc_utils
 from tests import cases
+import collections
 
 class Manifold():
     def __init__(self, problem_input):
-        self.lines = [list(l) for l in problem_input.splitlines()]
+        self.lines = problem_input.splitlines()
         self.split_count = 0
+        self.beams = collections.defaultdict(int)
 
     def print_manifold(self):
         [print(''.join(s)) for s in self.lines]
@@ -27,21 +29,27 @@ class Manifold():
                     self.lines[ypos + 1][xpos + 1] = "|"
 
     def scan_manifold(self):
-        for ypos in range(len(self.lines)):
-            for xpos in range(len(self.lines[0])):
-                x = self.lines[ypos][xpos]
+        self.beams[self.lines[0].index("S")] = 1
+        for ypos in range(len(self.lines)-1):
+            for xpos, count in list(self.beams.items()):
+                x = self.lines[ypos + 1][xpos]
                 if x == "^":
-                    if self.lines[ypos - 1][xpos] == "|":
-                        self.split_count += 1
+                    self.beams[xpos-1] += count
+                    self.beams[xpos+1] += count
+                    del self.beams[xpos]
+                    self.split_count += 1
 
 
 
 def answer(problem_input, level, test=False):
     manifold = Manifold(problem_input)
-    manifold.beam()
-    manifold.print_manifold()
+    # manifold.beam()
+    # manifold.print_manifold()
     manifold.scan_manifold()
-    return manifold.split_count
+    if level == 1:
+        return manifold.split_count
+    else:
+        return sum(manifold.beams.values())
 
 
 aoc_utils.run(answer, cases)
