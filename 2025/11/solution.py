03b53a35a6e7facc7bfad1aa377d2a499f1384a0
Logan McDonald <loganmcdona11@gmail.com>
2025-12-11 10:43:39 -0800

    giving up on part 2

--- 2025/11/solution.py
+++ 2025/11/solution.py
@@ -1,36 +1,52 @@
 from aoc_utils import aoc_utils
 from tests import cases
 
-# Classic recursive depth first search
-def find_all_paths(graph, start, end, path=[]):
-    path = path + [start]
+def count_paths(graph, start, end, required_nodes=None, visited=None, found_required=None):
+    if required_nodes is None:
+        required_nodes = set()
+    if visited is None:
+        visited = set()
+    if found_required is None:
+        found_required = set()
+    
+    if start in visited:
+        return 0
+    
+    new_visited = visited | {start}
+    new_found = found_required | ({start} if start in required_nodes else set())
+    
     if start == end:
-        return [path]
-
+        return 1 if required_nodes.issubset(new_found) else 0
+    
     if start not in graph:
-        return []
-
-    all_paths = []
+        return 0
+    
+    result = 0
     for node in graph[start]:
-        if node not in path:
-            new_paths = find_all_paths(graph, node, end, path)
-            for new_path in new_paths:
-                all_paths.append(new_path)
-
-    return all_paths
+        if node not in new_visited:
+            result += count_paths(
+                graph, node, end, required_nodes, new_visited, new_found
+            )
+    
+    return result
 
 
 def answer(problem_input, level, test=False):
     device_map = {}
-    start_node = "you"
+    if level == 1:
+        start_node = "you"
+    else:
+        start_node = "svr"
     end_node = "out"
     for line in problem_input.splitlines():
         device, output = line.split(": ")
         output = output.split()
         device_map[device] = output
-    
-    paths = find_all_paths(device_map, start_node, end_node)
-    return len(paths)
+
+    if level == 1:
+        return count_paths(device_map, start_node, end_node)
+    else:
+        return count_paths(device_map, start_node, end_node, {"dac", "fft"})
 
 
 aoc_utils.run(answer, cases)
--- 2025/11/tests.py
+++ 2025/11/tests.py
@@ -9,14 +9,17 @@ fff: out
 ggg: out
 hhh: ccc fff iii
 iii: out""", 'output': 5},
-#     {'level': 2, 'input': """aaa: you hhh
-# you: bbb ccc
-# bbb: ddd eee
-# ccc: ddd eee fff
-# ddd: ggg
-# eee: out
-# fff: out
-# ggg: out
-# hhh: ccc fff iii
-# iii: out""", 'output': None},
+    {'level': 2, 'input': """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out""", 'output': 2},
 ]
