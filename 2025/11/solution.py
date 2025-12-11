41fe3eb12597f74de0fa32e64d452135474f7e33
Logan McDonald <loganmcdona11@gmail.com>
2025-12-11 08:05:19 -0800

    classic dfs

--- 2025/11/solution.py
+++ 2025/11/solution.py
@@ -1,9 +1,36 @@
 from aoc_utils import aoc_utils
 from tests import cases
 
+# Classic recursive depth first search
+def find_all_paths(graph, start, end, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+
+    if start not in graph:
+        return []
+
+    all_paths = []
+    for node in graph[start]:
+        if node not in path:
+            new_paths = find_all_paths(graph, node, end, path)
+            for new_path in new_paths:
+                all_paths.append(new_path)
+
+    return all_paths
+
 
 def answer(problem_input, level, test=False):
-    return 0
+    device_map = {}
+    start_node = "you"
+    end_node = "out"
+    for line in problem_input.splitlines():
+        device, output = line.split(": ")
+        output = output.split()
+        device_map[device] = output
+    
+    paths = find_all_paths(device_map, start_node, end_node)
+    return len(paths)
 
 
 aoc_utils.run(answer, cases)
--- 2025/11/tests.py
+++ 2025/11/tests.py
@@ -1,3 +1,22 @@
 cases = [
-    {'level': 1, 'input': None, 'output': None},
+    {'level': 1, 'input': """aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out""", 'output': 5},
+#     {'level': 2, 'input': """aaa: you hhh
+# you: bbb ccc
+# bbb: ddd eee
+# ccc: ddd eee fff
+# ddd: ggg
+# eee: out
+# fff: out
+# ggg: out
+# hhh: ccc fff iii
+# iii: out""", 'output': None},
 ]
